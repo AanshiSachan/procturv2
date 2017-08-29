3a7525762311c9c672a438ca2bab79ca8e97f0f9
ronit123321 <ronit.oommen@gmail.com>
2017-08-29 18:28:46 +0530

    update for add enquiry and edit enquiry

--- src/app/custom/custom-add-enquiry/custom-add-enquiry.component.ts
+++ src/app/custom/custom-add-enquiry/custom-add-enquiry.component.ts
@@ -1,51 +1,45 @@
-import { Component, ViewChild, Input, OnInit, Output } from '@angular/core';
+import { Component,OnInit, trigger, transition, style, animate, state } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'custom-add-enquiry',
   templateUrl: './custom-add-enquiry.component.html',
-  styleUrls: ['./custom-add-enquiry.component.css']
+  styleUrls: ['./custom-add-enquiry.component.css'],
+  animations: [
+    trigger(
+      'Collapse',
+      [
+        transition(
+        ':enter', [
+          style({transform: 'translateY(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateY(0)', 'opacity': 1}))
+        ]
+      ),
+      transition(
+        ':leave', [
+          style({transform: 'translateY(0)', 'opacity': 1}),
+          animate('500ms', style({transform: 'translateY(100%)', 'opacity': 0})),
+        ]
+      )]
+    )
+  ],
 })
 export class CustomAddEnquiryComponent implements OnInit {
 
   source: LocalDataSource;
-
-  @ViewChild('modal')
-  modal: CustomAddEnquiryComponent;
-
   constructor() { }
 
   ngOnInit() { }
+  active:boolean = true;
+  Genders= [
+    {value: 'm', viewValue: 'Male'},
+    {value: 'f', viewValue: 'Female'},
+    {value: 'na', viewValue: 'Not Applicable'}
+  ];
 
-  open(size: string) {
-    this.modal.open(size);
+  toggleForm(){
+    this.active = !this.active;
+    console.log(this.active);
   }
-
-  openModal() {
-    alert('added');
-  }
-
-/*   onSubmit({ value, valid }: { value: OrderRequest, valid: boolean }) {
-    this.add({ value, valid });
-    this.modal.close();
-  }
- */
-  close() {
-    this.modal.close();
-  }
-
-/*   add({ value, valid }: { value: OrderRequest, valid: boolean }): void {
-    let result = JSON.stringify(value);
-    if (!result) {
-      return;
-    }
-    this.appService.create(value)
-      .then(orderRequest => {
-        this.orderRequests.push(orderRequest);
-        this.source.add(orderRequest);
-        this.source.refresh();
-      });
-  } */
-
 }
--- src/app/custom/custom-edit-enquiry/custom-edit-enquiry.component.ts
+++ src/app/custom/custom-edit-enquiry/custom-edit-enquiry.component.ts
@@ -1,30 +1,46 @@
-import { Component, ViewChild, Input, OnInit, Output } from '@angular/core';
+import { Component,OnInit, trigger, transition, style, animate, state } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'app-custom-edit-enquiry',
   templateUrl: './custom-edit-enquiry.component.html',
-  styleUrls: ['./custom-edit-enquiry.component.css']
+  styleUrls: ['./custom-edit-enquiry.component.css'],
+  animations: [
+    trigger(
+      'Collapse',
+      [
+        transition(
+        ':enter', [
+          style({transform: 'translateY(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateY(0)', 'opacity': 1}))
+        ]
+      ),
+      transition(
+        ':leave', [
+          style({transform: 'translateY(0)', 'opacity': 1}),
+          animate('500ms', style({transform: 'translateY(100%)', 'opacity': 0})),
+        ]
+      )]
+    )
+  ],
 })
-export class CustomEditEnquiryComponent implements OnInit {
+export class CustomEditEnquiryComponent{
 
   source: LocalDataSource;
-  data: any;
-
-  @ViewChild('modal')
-  modal: CustomEditEnquiryComponent;
   constructor() { }
+
   ngOnInit() { }
-  openModal(data) {
-    alert(data);
+  active:boolean = true;
+  Genders= [
+    {value: 'm', viewValue: 'Male'},
+    {value: 'f', viewValue: 'Female'},
+    {value: 'na', viewValue: 'Not Applicable'}
+  ];
+
+  toggleForm(){
+    this.active = !this.active;
+    console.log(this.active);
   }
-  onSubmit() {
-    this.modal.close();
-  }
-  close() {
-    this.modal.close();
-  }
-  edit(){
-  }
+
 }
