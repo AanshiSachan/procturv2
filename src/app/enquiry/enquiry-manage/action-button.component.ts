7def28d63e621149578f76aee7e76e5d8a757d90
ronit123321 <ronit.oommen@gmail.com>
2017-09-04 11:43:46 +0530

    data table view completed

--- src/app/enquiry/enquiry-manage/action-button.component.ts
+++ src/app/enquiry/enquiry-manage/action-button.component.ts
@@ -1,69 +1,42 @@
-import { Component, Input, OnInit, HostListener, ElementRef} from '@angular/core';
+import { Component, Input, OnInit, HostListener, ElementRef } from '@angular/core';
 import { ViewCell } from 'ng2-smart-table';
 
 @Component({
-    template: `
+  template: `
   
-    <style>
+  <style>
     :host /deep/ .actions{
-      max-width: 50px !important;
-      min-width: 45px !important;
-      height: 5em;
+     max-width: 50px !important;
+     min-width: 45px !important;
+     height: 5em;
     }
-
     :host /deep/ .actions .mat-button-ripple mat-ripple{
       max-width: 40px !important;
       min-width: 20px !important;
     }
-
-    ::ng-deep .mat-menu-panel{
-      max-width: 30em !important;
-      overflow: visible !important;
-      position: relative !important;
-      left: 10em !important;
-      top: -5em !important;
-      box-shadow: none;
-    }
-
-    ::ng-deep .mat-menu-content{
-      background: #424242;
-      width: 30em;
-    }
-
-    ::ng-deep .mat-menu-item{
-      width: 5em !important;
-      display: inline !important;      
-    }
-
-    ::ng-deep button.mat-menu-item {
-      width: 5em !important;
-    }
-
-    </style>
-    
-
-
-    <button md-button [mdMenuTriggerFor]="menu" class="actions">
+  </style>
+  
+  <button md-button [mdMenuTriggerFor]="menu" class="actions" mdTooltip="Click to perform an action" mdTooltipPosition="above">
     <md-icon>flash_on</md-icon></button>
     <md-menu #menu="mdMenu" class="actionmenu">
    
-      <button md-menu-item>
+      <button md-menu-item mdTooltip="Update Enquiry" mdTooltipPosition="left">
         <md-icon>autorenew</md-icon>
       </button>
      
-      <button md-menu-item>
+      <button md-menu-item mdTooltip="Edit Details" mdTooltipPosition="left">
        <md-icon>border_color</md-icon>
       </button>
      
-      <button md-menu-item>
+      <button md-menu-item mdTooltip="Delete Enquiry" mdTooltipPosition="left">
        <md-icon>delete</md-icon>
       </button>
 
-      <button md-menu-item>
+      <button md-menu-item mdTooltip="Convert To Student" mdTooltipPosition="left">
        <md-icon>perm_identity</md-icon>
       </button>
 
-      <button md-menu-item>
+      <button md-menu-item mdTooltip="Pay Registration Fees" mdTooltipPosition="left">
        <md-icon>payment</md-icon>
       </button>
     </md-menu>
@@ -72,8 +45,8 @@ import { ViewCell } from 'ng2-smart-table';
 
 export class ActionButtonComponent implements OnInit {
 
-  private active:boolean = false;
-  constructor(){}
-  ngOnInit() {}
+  private active: boolean = false;
+  constructor() { }
+  ngOnInit() { }
 
 }
