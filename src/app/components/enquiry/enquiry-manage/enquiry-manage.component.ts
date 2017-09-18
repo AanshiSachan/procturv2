cdb2d4ce5ff7c44fb0c1a48bf320fb6c02326b52
ronit123321 <ronit.oommen@gmail.com>
2017-09-18 11:46:30 +0530

    Update to Table Filter and Toggle

--- src/app/components/enquiry/enquiry-manage/enquiry-manage.component.ts
+++ src/app/components/enquiry/enquiry-manage/enquiry-manage.component.ts
@@ -14,6 +14,9 @@ import { ActionButtonComponent } from './action-button.component';
 import { Router } from '@angular/router';
 declare var require: any;
 import { Logger } from '@nsalaun/ng-logger';
+import { instituteInfo } from '../../../model/instituteinfo';
+import * as moment from 'moment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-enquiry-manage',
@@ -23,8 +26,41 @@ import { Logger } from '@nsalaun/ng-logger';
 
 export class EnquiryManageComponent implements OnInit {
 
-  AdFilter:boolean; rows: any = []; optionsModel: any[]; checkedOpt: any[]; myOptions: IMultiSelectOption[]; source: LocalDataSource; gridSelected:any; busy: Subscription; selectedCol = [];
-  
+  AdFilter:boolean; rows: any = []; optionsModel = []; checkedOpt: any[]; myOptions: IMultiSelectOption[]; source: LocalDataSource; gridSelected:any; 
+  busy: Subscription; selectedCol = []; check: boolean = true; disable: boolean = true; checkedStatus =[]; filtered = [];
+  enqstatus: any = []; enqPriority: any = []; enqFollowType: any = []; enqAssignTo: any = []; enqStd: any = []; enqSub: any = []; enqScholarship: any = []; enqSub2: any = [];
+  updateDate: any; adFilterForm: FormGroup; 
+
+  formData: instituteInfo = {
+    institute_id: 100123, 
+    function_type: "manageSearchEnquiries", 
+    username: "31469|0",
+    password: "admin@123", 
+    onLoad: 0, 
+    name: "", 
+    phone: "", 
+    email: "", 
+    enquiry_no: "", 
+    priority: "", 
+    filtered_statuses: "", 
+    follow_type: "", 
+    followUpDate: moment().format('YYYY-MM-DD'), 
+    enquiry_date: "", 
+    assigned_to: -1, 
+    standard_id: -1, 
+    subject_id: -1, 
+    is_recent: "Y",
+    filtered_slots: "", 
+    isDashbord: "", 
+    enquireDateFrom: null, 
+    enquireDateTo: null,
+    updateDate: null, 
+    updateDateFrom: "", 
+    updateDateTo: "", 
+    pageNo: 1, 
+    sizeLimit: 100,
+  }
+
   stats = {
     All :       {value: 'All', prop: 'All', checked: true, disabled: false},
     Open :      {value: 'Open', prop: 'Open', checked: false, disabled: false}, 
@@ -32,16 +68,6 @@ export class EnquiryManageComponent implements OnInit {
     Admitted:   {value: 'Admitted', prop: 'Student Admitted', checked: false, disabled: false}, 
     Inactive :  {value: 'Inactive', prop: 'Converted', checked: false, disabled: false},
   };
-
-  check: boolean = true; disable: boolean = true;
-  checkedStatus =[];
-  filtered = [];
-
-  Status = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
 
   settings = {
     mode: 'external', hideSubHeader: false, selectMode: 'multi',
@@ -94,7 +120,10 @@ export class EnquiryManageComponent implements OnInit {
     },
   };
 
-  constructor(private enquire: FetchenquiryService, private dialog: MdDialog, public snackBar: MdSnackBar, private router: Router, private logger: Logger){
+  constructor(private enquire: FetchenquiryService, private dialog: MdDialog, public snackBar: MdSnackBar, private router: Router, private logger: Logger, private adfilterFB: FormBuilder){
+    this.adFilterForm = adfilterFB.group({
+      
+    })
   }
   
   ngOnInit() {
@@ -104,51 +133,80 @@ export class EnquiryManageComponent implements OnInit {
       this.source = new LocalDataSource(this.rows);
       this.source.refresh();
     })
-    
     this.myOptions = [{ name: 'Enquiry ID', id: 'institute_enquiry_id'}, { name: 'Institute ID', id: 'institution_id' }, { name: 'Enquiry No', id: 'enquiry_no' }, { name: 'Name', id: 'name' }, { name: 'Phone', id: 'phone' }, { name: 'Email', id: 'email' }, { name: 'Standard', id: 'standard' }, { name: 'Gender', id: 'Gender' }, { name: 'Subjects', id: 'subjects' }, { name: 'Status', id: 'status' }, { name: 'Status Value', id: 'statusValue' }, { name: 'is Converted', id: 'is_converted' }, { name: 'Follow up Date', id: 'followUpDate' }, { name: 'Occupation ID', id: 'occupation_id' }, { name: 'School ID', id: 'school_id' }, { name: 'Enquiry Date', id: 'enquiry_date' }, { name: 'Standard ID', id: 'standard_id' }, { name: 'Subject ID', id: 'subject_id' }, { name: 'Reffered by', id: 'referred_by' }, { name: 'Source ID', id: 'source_id' }, { name: 'Priority', id: 'priority' }, { name: 'Follow type', id: 'follow_type' }, { name: 'Assigned Name', id: 'assigned_name' }, { name: 'Is recent', id: 'is_recent' }, { name: 'Slot ID', id: 'slot_id' }, { name: 'Slot', id: 'slot' }, { name: 'Update Date', id: 'updateDate' }, { name: 'Is Dashboard', id: 'isDashbord' }, { name: 'Is Report', id: 'isRport' }, { name: 'Total Count', id: 'totalcount' }, { name: 'New Enquiry Count', id: 'newEnqcount' }, { name: 'Enquiry Date', id: 'enquiry_no_date' }, { name: 'Person Name', id: 'name_person' }, { name: 'Follow up date', id: 'followUpDateTime' }, { name: 'Standard subject', id: 'standard_subject' }, { name: 'Closed Reason', id: 'closedReasonText' }, { name: 'Follow up Time', id: 'followUpTime' }, { name: 'Filtered Status', id: 'filtered_statuses' }, { name: 'Filtered Slot', id: 'filtered_slots' }];
+    this.myOptions.forEach  (el => 
+      {
+      if(this.settings.columns[el.id].show){
+        this.optionsModel.push(el.id);
+      }
+    })
+    this.FetchEnquiryPrefilledData();    
+    this.logger.group("ngOnit");
+    this.logger.log("initialized Http Calls for fetching form prefilled data and table Data");
+    this.logger.log(this.optionsModel);
+    this.logger.debug("Completed Http Calls");
+    this.logger.groupEnd();
+  }
+
+  FetchEnquiryPrefilledData(){
+    this.enquire.getEnqStatus().subscribe(
+      data => {this.enqstatus = data;},
+      err => { console.log(err); }
+    );
+    this.enquire.getEnqPriority().subscribe(
+      data => { this.enqPriority = data; },
+      err => { console.log(err); }
+    );
+    this.enquire.getFollowupType().subscribe(
+      data => { this.enqFollowType = data },
+      err => { console.log(err); }
+    );
+    this.enquire.getAssignTo().subscribe(
+      data => { this.enqAssignTo = data; },
+      err => { console.log(err); }
+    );
+    this.enquire.getScholarPrefillData().subscribe(
+      data => { 
+        //console.log(data);
+        data.forEach(el => {
+          if(el.label == "Scholarship"){
+            //console.log(el);
+            this.enqScholarship = el.prefilled_data.split(',');
+          }
+          else if(el.label == "Subject2"){
+            this.enqSub2 = el.prefilled_data.split(',');
+          }
+        })
+      },
+      err => { console.log(err); }
+    );
+    this.enquire.getEnqStardards().subscribe(
+      data => { this.enqStd = data;},
+      err => { console.log(err); }
+    );
   }
 
   toggleOptionChange(ev){
+    console.log(ev);
+    console.log(this.optionsModel);
     this.settings = {
       mode: 'external', hideSubHeader: false, selectMode: 'multi',
       actions: { add: false, edit: false, delete: false, columnTitle: '',},
       columns: {
-        enquiry_no: { title: 'Enquiry No.', filter: false, show: true }, 
-        enquiry_date: { title: 'Enquiry Date', filter: false, show: true },
-        name: { title: 'Student Name', filter: false, show: true }, 
-        phone: { title: 'Contact No.', filter: false, show: true }, 
-        priority: { title: 'Priority', filter: false, show: true },
-        follow_type: { title: 'Follow type', filter: false, show: true },
-        followUpDate: { title: 'Follow up Date', filter: false, show: true },      
-        assigned_name: { title: 'Assigned To', filter: false, show: true }, 
-        standard: { title: 'Standard', filter: false, show: true },
-        subjects: { title: 'Subjects', filter: false, show: true }, 
-        action: { title: 'Action', filter: false, type: 'custom', renderComponent:  ActionButtonComponent },
-        updateDate: { title: 'Update Date', filter: false, show: true },
-        statusValue: { title: 'Status Value', filter: false, show: true },
-        enquiry_no_date: { title: 'Enquiry No. & Date', filter: false, show: false },followUpTime: { title: 'Follow up Time', filter: false, show: false },followUpDateTime: { title: 'Follow up dateTime', filter: false, show: false },filtered_statuses: { title: 'Filtered Status', filter: false, show: false },email: { title: 'Email', filter: false, show: false }, referred_by: { title: 'Reffered By', filter: false, show: false}, status: { title: 'Status', filter: false, show: false }, institute_enquiry_id: { title: 'Enquiry ID', filter: false, show: false }, institution_id: { title: 'Institute ID', filter: false, show: false }, Gender: { title: 'Gender', filter: false, show: false }, is_converted: { title: 'Is Converted', filter: false, show: false }, occupation_id: { title: 'Occupation ID', filter: false, show: false }, school_id: { title: 'School ID', filter: false, show: false }, standard_id: { title: 'Standard ID', filter: false, show: false }, subject_id: { title: 'Subject ID', filter: false, show: false }, source_id: { title: 'Source ID', filter: false, show: false }, is_recent: { title: 'Is recent', filter: false, show: false }, slot_id: { title: 'Slot ID', filter: false, show: false }, slot: { title: 'Slot', filter: false, show: false }, isDashbord: { title: 'Is Dashboard', filter: false, show: false }, isRport: { title: 'Is Report', filter: false, show: false }, totalcount: { title: 'Total Count', filter: false, show: false }, newEnqcount: { title: 'New Enquiry Count', filter: false, show: false },  name_person: { title: 'Person Name', filter: false, show: false }, standard_subject: { title: 'Standard subject', filter: false, show: false }, closedReasonText: { title: 'Closed Reason', filter: false, show: false },  filtered_slots: { title: 'Filtered Slot', filter: false, show: false },
+        enquiry_no: { title: 'Enquiry No.', filter: false, show: false }, enquiry_date: { title: 'Enquiry Date', filter: false, show: false }, name: { title: 'Student Name', filter: false, show: false }, phone: { title: 'Contact No.', filter: false, show: false }, priority: { title: 'Priority', filter: false, show: false }, follow_type: { title: 'Follow type', filter: false, show: false }, followUpDate: { title: 'Follow up Date', filter: false, show: false }, assigned_name: { title: 'Assigned To', filter: false, show: false }, standard: { title: 'Standard', filter: false, show: false }, subjects: { title: 'Subjects', filter: false, show: false }, action: { title: 'Action', filter: false, type: 'custom', renderComponent:  ActionButtonComponent },updateDate: { title: 'Update Date', filter: false, show: false },statusValue: { title: 'Status Value', filter: false, show: false },enquiry_no_date: { title: 'Enquiry No. & Date', filter: false, show: false },followUpTime: { title: 'Follow up Time', filter: false, show: false },followUpDateTime: { title: 'Follow up dateTime', filter: false, show: false },filtered_statuses: { title: 'Filtered Status', filter: false, show: false },email: { title: 'Email', filter: false, show: false }, referred_by: { title: 'Reffered By', filter: false, show: false}, status: { title: 'Status', filter: false, show: false }, institute_enquiry_id: { title: 'Enquiry ID', filter: false, show: false }, institution_id: { title: 'Institute ID', filter: false, show: false }, Gender: { title: 'Gender', filter: false, show: false }, is_converted: { title: 'Is Converted', filter: false, show: false }, occupation_id: { title: 'Occupation ID', filter: false, show: false }, school_id: { title: 'School ID', filter: false, show: false }, standard_id: { title: 'Standard ID', filter: false, show: false }, subject_id: { title: 'Subject ID', filter: false, show: false }, source_id: { title: 'Source ID', filter: false, show: false }, is_recent: { title: 'Is recent', filter: false, show: false }, slot_id: { title: 'Slot ID', filter: false, show: false }, slot: { title: 'Slot', filter: false, show: false }, isDashbord: { title: 'Is Dashboard', filter: false, show: false }, isRport: { title: 'Is Report', filter: false, show: false }, totalcount: { title: 'Total Count', filter: false, show: false }, newEnqcount: { title: 'New Enquiry Count', filter: false, show: false },  name_person: { title: 'Person Name', filter: false, show: false }, standard_subject: { title: 'Standard subject', filter: false, show: false }, closedReasonText: { title: 'Closed Reason', filter: false, show: false },  filtered_slots: { title: 'Filtered Slot', filter: false, show: false },
       },
       pager: {
         display: false,
       },
     };
     this.settingUpdater = Object.assign({}, this.settings);
-    ev.forEach(el => {
-      this.settingUpdater.columns[el].show = !this.settingUpdater.columns[el].show;
-      this.snackBar.open(this.settingUpdater.columns[el].title, "Updated", {
-        duration: 1000,
-        });
+    this.optionsModel.forEach(el => {
+      this.settingUpdater.columns[el].show = true;
     })
     this.settings = Object.assign({}, this.settingUpdater);
-  }
-
-  toggle(ele) {
-    console.log(ele);
-    this.settingUpdater.columns[ele].show = !this.settingUpdater.columns[ele].show;
-    this.settings = Object.assign({}, this.settingUpdater);
-    this.snackBar.open(this.settingUpdater.columns[ele].title, "Updated", {
-    duration: 1000,
-    });
+    this.snackBar.open("Table Updated", "Ok", {
+        duration: 1000,
+        });
   }
 
   statusFilter(checkerObj){
@@ -232,6 +290,8 @@ export class EnquiryManageComponent implements OnInit {
 
   openAdFilter(){
     this.AdFilter =  true;
+    console.log(this.formData);
+    //let instituteFormData = `institute_id=${this.instituteData.institute_id}&function_type=${this.instituteData.function_type}&username=${this.instituteData.username}&password=${this.instituteData.password}&onLoad=${this.instituteData.onLoad}&name=${this.instituteData.name}&phone=${this.instituteData.phone}&email=${this.instituteData.email}&enquiry=${this.instituteData.enquiry_no}&priority=${this.instituteData.priority}&filtered_statuses=${this.instituteData.filtered_statuses}&follow_type=${this.instituteData.follow_type}&followUpDate=${this.instituteData.followUpDate}&enquiry_date=${this.instituteData.enquiry_date}&assigned_to=${this.instituteData.assigned_to}&standard_id=${this.instituteData.standard_id}&subject_id=${this.instituteData.subject_id}&is_recent=${this.instituteData.is_recent}&filtered_slots=${this.instituteData.filtered_slots}&isDashbord=${this.instituteData.isDashbord}&enquireDateFrom=${this.instituteData.enquireDateFrom}&enquireDateTo=${this.instituteData.enquireDateTo}&updateDate=${this.instituteData.updateDate}&updateDateFrom=${this.instituteData.updateDateFrom}&updateDateTo=${this.instituteData.updateDateTo}&page=${this.instituteData.pageNo}&size=${this.instituteData.sizeLimit}`;
   }
   
   closeAdFilter(){
@@ -284,4 +344,20 @@ export class EnquiryManageComponent implements OnInit {
     this.gridSelected = event.selected;
   }
 
+  filterAdvanced(event){
+    console.log(event);
+    if(this.validate(event)){
+      /* Perform Filter Task  
+          Perform Http Request and Send Data via Post
+      */
+    }
+    else{
+      console.log("Please revalidate the submitted data");
+    }
+  }
+
+  validate(formdata): boolean{
+       
+    return false;
+  }
 }
--- src/app/services/fetchenquiry.service.ts
+++ src/app/services/fetchenquiry.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Request, Headers, XHRBackend } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { instituteInfo } from '../model/instituteinfo';
-import {EnquiryCampaign} from '../model/enquirycampaign';
+import { EnquiryCampaign } from '../model/enquirycampaign';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import 'rxjs/Rx';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 import * as moment from 'moment';
 import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 
@@ -14,38 +14,67 @@ import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 export class FetchenquiryService {
 
   instituteData: instituteInfo = {
-    institute_id: 100123,function_type: "manageSearchEnquiries",username: "31469|0",
-    password: "admin@123",onLoad: 0,name: "",phone: "",
-    email: "",enquiry_no: "",priority: "",filtered_statuses: "",follow_type: "",
-    followUpDate: moment().format('YYYY-MM-DD'),enquiry_date: "",assigned_to: -1,standard_id: -1,subject_id: -1,is_recent: "Y",
-    filtered_slots: "",isDashbord: "",enquireDateFrom: "",enquireDateTo: "",
-    updateDate: "",updateDateFrom: "",updateDateTo: "",
+    institute_id: 100123, function_type: "manageSearchEnquiries", username: "31469|0",
+    password: "admin@123", onLoad: 0, name: "", phone: "",
+    email: "", enquiry_no: "", priority: "", filtered_statuses: "", follow_type: "",
+    followUpDate: moment().format('YYYY-MM-DD'), enquiry_date: "", assigned_to: -1, standard_id: -1, subject_id: -1, is_recent: "Y",
+    filtered_slots: "", isDashbord: "", enquireDateFrom: "", enquireDateTo: "",
+    updateDate: "", updateDateFrom: "", updateDateTo: "",
     pageNo: 1, sizeLimit: 100,
   }
 
-  url: string;
-  urlCampaign: string;
-  Authorization: string;
-  headers: Headers;
-  headersCampaign: Headers;
-  instituteFormData: any = {};
-  row: any = [];
-  filtered = [];
+  url: string; urlCampaign: string; urlAssignTo: string; urlScholarSub: string;
+  urlEnqsta: string; urlFollType: string; urlEnqPri: string; urlStdSub: string; urlSubject: string;
+  Authorization: string; headers: Headers; headersCampaign: Headers; headersAssignTo: Headers;
+  headersScholarSub: Headers; headersEnqsta: Headers; headersFollType: Headers; headersEnqPri: Headers;
+  headersStdSub: Headers; headersSubject: Headers; instituteFormData: any = {}; row: any = []; filtered = [];
   daterange = {
     updateDateFrom: moment().startOf('month').format('YYYY-MM-DD'),
     updateDateTo: moment().format('YYYY-MM-DD'),
   };
 
   constructor(private http: Http) {
-    this.url = "https://app.proctur.com/StdMgmtWebAPI/api/v1/enquiry/dashboard/100123";
-    this.urlCampaign = 'https://app.proctur.com/CampaignServlet';
-    this.instituteFormData = `institute_id=${this.instituteData.institute_id}&function_type=${this.instituteData.function_type}&username=${this.instituteData.username}&password=${this.instituteData.password}&onLoad=${this.instituteData.onLoad}&name=${this.instituteData.name}&phone=${this.instituteData.phone}&email=${this.instituteData.email}&enquiry=${this.instituteData.enquiry_no}&priority=${this.instituteData.priority}&filtered_statuses=${this.instituteData.filtered_statuses}&follow_type=${this.instituteData.follow_type}&followUpDate=${this.instituteData.followUpDate}&enquiry_date=${this.instituteData.enquiry_date}&assigned_to=${this.instituteData.assigned_to}&standard_id=${this.instituteData.standard_id}&subject_id=${this.instituteData.subject_id}&is_recent=${this.instituteData.is_recent}&filtered_slots=${this.instituteData.filtered_slots}&isDashbord=${this.instituteData.isDashbord}&enquireDateFrom=${this.instituteData.enquireDateFrom}&enquireDateTo=${this.instituteData.enquireDateTo}&updateDate=${this.instituteData.updateDate}&updateDateFrom=${this.instituteData.updateDateFrom}&updateDateTo=${this.instituteData.updateDateTo}&page=${this.instituteData.pageNo}&size=${this.instituteData.sizeLimit}`;
     this.Authorization = "MzE0Njl8MDphZG1pbkAxMjM6MTAwMTIz";
+    this.instituteFormData = `institute_id=${this.instituteData.institute_id}&function_type=${this.instituteData.function_type}&username=${this.instituteData.username}&password=${this.instituteData.password}&onLoad=${this.instituteData.onLoad}&name=${this.instituteData.name}&phone=${this.instituteData.phone}&email=${this.instituteData.email}&enquiry=${this.instituteData.enquiry_no}&priority=${this.instituteData.priority}&filtered_statuses=${this.instituteData.filtered_statuses}&follow_type=${this.instituteData.follow_type}&followUpDate=${this.instituteData.followUpDate}&enquiry_date=${this.instituteData.enquiry_date}&assigned_to=${this.instituteData.assigned_to}&standard_id=${this.instituteData.standard_id}&subject_id=${this.instituteData.subject_id}&is_recent=${this.instituteData.is_recent}&filtered_slots=${this.instituteData.filtered_slots}&isDashbord=${this.instituteData.isDashbord}&enquireDateFrom=${this.instituteData.enquireDateFrom}&enquireDateTo=${this.instituteData.enquireDateTo}&updateDate=${this.instituteData.updateDate}&updateDateFrom=${this.instituteData.updateDateFrom}&updateDateTo=${this.instituteData.updateDateTo}&page=${this.instituteData.pageNo}&size=${this.instituteData.sizeLimit}`;
+
+    this.url = "https://app.proctur.com/StdMgmtWebAPI/api/v1/enquiry/dashboard/100123";
     this.headers = new Headers();
     this.headers.append("Content-Type", "application/json");
     this.headers.append("Authorization", this.Authorization);
+
+    this.urlCampaign = 'https://app.proctur.com/CampaignServlet';
     this.headersCampaign = new Headers();
     this.headersCampaign.append("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+    this.urlAssignTo = "https://app.proctur.com/StdMgmtWebAPI/api/v1/profiles/100123";
+    this.headersAssignTo = new Headers();
+    this.headersAssignTo.append("Authorization", this.Authorization);
+    this.headersAssignTo.append("Content-Type", "application/json");
+
+    this.urlScholarSub = "https://app.proctur.com/StdMgmtWebAPI/api/v1/enquiry/fetchCustomEnquiryComponents/100123?id=0&isSearhable=Y&page=1";
+    this.headersScholarSub = new Headers();
+    this.headersScholarSub.append("Authorization", this.Authorization);
+    this.headersScholarSub.append("Content-Type", "application/json");
+
+    this.urlEnqsta = "https://app.proctur.com/StdMgmtWebAPI/api/v1/masterData/type/SPEC_ENQUIRY_STATUS";
+    this.headersEnqsta = new Headers();
+    this.headersEnqsta.append("Authorization", this.Authorization);
+    this.headersEnqsta.append("Content-Type", "application/json");
+
+    this.urlEnqPri = "https://app.proctur.com/StdMgmtWebAPI/api/v1/masterData/type/ENQ_PRIORITY";
+    this.headersEnqPri = new Headers();
+    this.headersEnqPri.append("Authorization", this.Authorization);
+    this.headersEnqPri.append("Content-Type", "application/json");
+
+    this.urlFollType = "https://app.proctur.com/StdMgmtWebAPI/api/v1/masterData/type/ENQ_FOLLOW_TYPE";
+    this.headersFollType = new Headers();
+    this.headersFollType.append("Authorization", this.Authorization);
+    this.headersFollType.append("Content-Type", "application/json");
+
+    this.urlStdSub = "https://app.proctur.com/StdMgmtWebAPI/api/v1/standards/all/100123?active=Y";
+    this.headersStdSub = new Headers();
+    this.headersStdSub.append("Authorization", this.Authorization);
+    this.headersStdSub.append("Content-Type", "application/json");
   }
 
   loadenquiry(daterange): Observable<any> {
@@ -54,22 +83,63 @@ export class FetchenquiryService {
       .map(result => result.json());
   }
 
-  getAllEnquiry(): Observable<EnquiryCampaign[]>{
+  getAllEnquiry(): Observable<EnquiryCampaign[]> {
     return this.http.post(this.urlCampaign, this.instituteFormData, { headers: this.headersCampaign })
       .map(res => {
-        this.row = res.json().aaData; 
-      return this.row;
+        this.row = res.json().aaData;
+        return this.row;
       });
   }
 
-  getEnquiry(): Observable<EnquiryCampaign[]>{
-    return this.http.post(this.urlCampaign, this.instituteFormData, { headers: this.headersCampaign })
+  getAssignTo(): any {
+    let content = JSON.stringify({ "user_Type": 0 });
+    return this.http.post(this.urlAssignTo, content, { headers: this.headersAssignTo })
       .map(res => {
-      return res.json().aaData
-    }).do(data => data.array.forEach(element => {
-      console.log(element);
-    }))
+        return res.json()
+      });
+  }
+
+  getScholarPrefillData(): any {
+    return this.http.get(this.urlScholarSub, { headers: this.headersScholarSub })
+      .map(res => {
+        return res.json();
+      })
+  }
+
+  getEnqStatus(): any{
+    return this.http.get(this.urlEnqsta, { headers: this.headersEnqsta })
+      .map(res => {
+        return res.json();
+      })
+  }
+
+  getEnqPriority(): any {
+    return this.http.get(this.urlEnqPri, { headers: this.headersEnqPri })
+      .map(res => {
+        return res.json();
+      })
+  }
+
+  getFollowupType(): any {
+    return this.http.get(this.urlFollType, { headers: this.headersFollType })
+      .map(res => {
+        return res.json();
+      })
+  }
+
+  getEnqStardards(): any{
+    return this.http.get(this.urlStdSub, { headers: this.headersStdSub })
+    .map(res => {
+      return res.json();
+    })
+  }
+
+  getEnqSubjects(id): any{
+    this.urlSubject= "https://app.proctur.com/StdMgmtWebAPI/api/v1/subjects/standards/" +id;
+    this.headersSubject = new Headers();
+    this.headersSubject.append("Authorization", this.Authorization);
+    this.headersSubject.append("Content-Type", "application/json");
+    this.http.get(this.urlSubject, {headers: this.headersSubject})
   }
 
 }
-  
