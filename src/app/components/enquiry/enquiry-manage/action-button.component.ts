585b7ed74bd89f88cebc1e41a01f3f615e79d018
ronit123321 <ronit.oommen@gmail.com>
2017-09-21 18:59:36 +0530

    ButtonRender Component added

--- src/app/components/enquiry/enquiry-manage/action-button.component.ts
+++ src/app/components/enquiry/enquiry-manage/action-button.component.ts
@@ -2,51 +2,248 @@ import { Component, Input, OnInit, HostListener, ElementRef } from '@angular/cor
 import { ViewCell } from 'ng2-smart-table';
 
 @Component({
-  template: `
-  
+  template:
+  `
   <style>
-    :host /deep/ .actions{
-     max-width: 50px !important;
-     min-width: 45px !important;
-     height: 5em;
+
+   .table-action-icon{
+     max-height: 50px;
+     max-width: 50px;
+     background: none;
+     text-decoration: none;
+     outline: none;
+     border: none;
+   }
+   .enquiry-action {
+    position: relative;
+    cursor: pointer;
+    .cls-1,
+    .cls-2 {
+        fill: none;
+        stroke: transparent;
     }
-    :host /deep/ .actions .mat-button-ripple mat-ripple{
-      max-width: 40px !important;
-      min-width: 20px !important;
+    svg {
+        width: 18px;
     }
-  </style>
+    .cls-2 {
+        stroke: #7d7f80;
+        stroke-miterlimit: 10;
+    }
+   }
+   .action-menu{
+    position: absolute;
+    background: #fff;
+    width: 350px;
+    border-radius: 0;
+    border: 1px solid #ccc;
+    bottom: 15px;
+    box-shadow: 0px 2px 4px 1px #ccc;
+    transform: translateX(-50%);
+    left: 50%;
+   }
   
-  <button md-button [mdMenuTriggerFor]="menu" class="actions" mdTooltip="Click to perform an action" mdTooltipPosition="above">
-    <md-icon>flash_on</md-icon></button>
-    <md-menu #menu="mdMenu" class="actionmenu">
-   
-      <button md-menu-item mdTooltip="Update Enquiry" mdTooltipPosition="left">
-        <md-icon>autorenew</md-icon>
-      </button>
-     
-      <button md-menu-item mdTooltip="Edit Details" mdTooltipPosition="left">
-       <md-icon>border_color</md-icon>
-      </button>
-     
-      <button md-menu-item mdTooltip="Delete Enquiry" mdTooltipPosition="left">
-       <md-icon>delete</md-icon>
-      </button>
+   .action-menu-inner ul {
+    font-size: 0;
+    position: relative;
+    padding: 5px 5px;
+   }
+   .action-menu-inner ul:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: -15px;
+    margin: auto;
+    border-top: 8px solid #fff;
+    border-bottom: 8px solid transparent;
+    border-right: 8px solid transparent;
+    border-left: 8px solid transparent;
+    width: 0;
+    height: 0;
+   }
+   .action-menu-inner ul li {
+    display: inline-block;
+    text-align: center;
+    vertical-align: top;
+    font-size: 12px;
+    padding: 0 8px;
+    box-sizing: border-box;
+    cursor: pointer;
+    width: 18%;
+    &:last-child {
+        width: 28%;
+        svg {
+            width: 33px;
+        }
+    }
+    &.edit-detail-icon {
+        svg * {
+            fill: none;
+            stroke: #8b8b8b;
+        }
+        svg .cls-1 {
+            stroke: none;
+    
+        }
+    }
+    &:hover {
+        .cls-2,
+        .cls-3 {
+            fill: none;
+            stroke: $common-blue;
+        }
+        color: $common-blue;
+    }
+   }
+   .action-menu-inner ul li span {
+    display: block;
+    font-size: 9px;
+    text-align: center;
+    line-height: 10px;
+   }
+   .action-menu-inner i {
+    display: block;
+    height: 32px;
+    svg{
+      width:28px;
+    }
+   }
+   .action-icon{
+     width: 28px;
+   }
+  </style>
+  <div class="enquiry-action">
+  <div>
+  <button class="btn table-action-icon"  (click)="openMenu($event)" >
+    <img src="./assets/images/action_hover.svg" height="20" width="20">
+  </button>
+  <div class="action-menu" *ngIf="showMenu" (mouseleave)="closeMenu()">
+  <div class="action-menu-inner">
+      <ul>
+          <li>
+              <i>
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="11716 358 26 22" class="action-icon">
+                  <g id="Group_1238" data-name="Group 1238" transform="translate(10862 -6)">
+                      <rect id="Rectangle_393" data-name="Rectangle 393" class="cls-1" width="26" height="22" transform="translate(854 364)"/>
+                      <g id="Group_791" data-name="Group 791" transform="translate(857 365)">
+                      <path id="Path_210" data-name="Path 210" class="cls-2" d="M3.6,3.7l3,.263L6.334,6.907" transform="translate(-2.195 -2.269)"/>
+                      <path id="Path_211" data-name="Path 211" class="cls-2" d="M13,1.673A10.094,10.094,0,0,1,24.355,4.512,10,10,0,0,1,23.146,18.6" transform="translate(-6.653 -0.978)"/>
+                      <path id="Path_212" data-name="Path 212" class="cls-2" d="M14.634,21.811A9.994,9.994,0,0,1,4.488,4.883L5.276,4.2" transform="translate(-0.927 -2.506)"/>
+                      <path id="Path_213" data-name="Path 213" class="cls-2" d="M33.7,33.407l-3-.263.263-2.944" transform="translate(-15.048 -14.838)"/>
+                      <line id="Line_157" data-name="Line 157" class="cls-2" x1="0.789" y2="0.683" transform="translate(15.652 17.623)"/>
+                      </g>
+                  </g>
+              </svg>    
+              </i>
+              <span>
+                  Update <br>Enquiry
+              </span>
+          </li>
+          <li class="edit-detail-icon">
+              <i>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="11778 358 26 22" class="action-icon">
 
-      <button md-menu-item mdTooltip="Convert To Student" mdTooltipPosition="left">
-       <md-icon>perm_identity</md-icon>
-      </button>
+              <g id="Group_1234" data-name="Group 1234" transform="translate(10862 -6)">
+                  <rect id="Rectangle_697" data-name="Rectangle 697" class="cls-1" width="26" height="22" transform="translate(916 364)"/>
+                  <g id="Group_1126" data-name="Group 1126" transform="translate(-0.478 -0.386)">
+                  <path id="Path_215" data-name="Path 215" class="cls-2" d="M0,0H17.753" transform="translate(920.478 385.386)"/>
+                  <path id="Path_214" data-name="Path 214" class="cls-3" d="M8.492,16.068,3,17.621l1.553-5.492,10.6-10.6a1.877,1.877,0,0,1,2.718,0L19.089,2.7a1.877,1.877,0,0,1,0,2.718Z" transform="translate(919.142 364.436)"/>
+                  <line id="Line_159" data-name="Line 159" class="cls-3" x1="3.883" y1="3.883" transform="translate(932.738 367.578)"/>
+                  </g>
+              </g>
+              </svg>    
+                  
+              </i>
+              <span>
+                  Edit <br> Details
+              </span>
+          </li>
+          <li>
+              <i>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="11841 358 26 22" class="action-icon">
 
-      <button md-menu-item mdTooltip="Pay Registration Fees" mdTooltipPosition="left">
-       <md-icon>payment</md-icon>
-      </button>
-    </md-menu>
-    `,
+                  <g id="Group_1237" data-name="Group 1237" transform="translate(10862 -6)">
+                      <rect id="Rectangle_698" data-name="Rectangle 698" class="cls-1" width="26" height="22" transform="translate(979 364)"/>
+                      <g id="Group_795" data-name="Group 795" transform="translate(981 365)">
+                      <path id="Path_216" data-name="Path 216" class="cls-2" d="M20.294,6V20.706a2.36,2.36,0,0,1-2.353,2.353H7.353A2.36,2.36,0,0,1,5,20.706V6" transform="translate(-2.059 -3.059)"/>
+                      <line id="Line_160" data-name="Line 160" class="cls-2" x2="21.176" transform="translate(0 2.941)"/>
+                      <path id="Path_217" data-name="Path 217" class="cls-2" d="M19.059,3.941H12V2.176A1.18,1.18,0,0,1,13.176,1h4.706a1.18,1.18,0,0,1,1.176,1.176Z" transform="translate(-4.941 -1)"/>
+                      <line id="Line_161" data-name="Line 161" class="cls-2" y2="11.176" transform="translate(10.588 5.882)"/>
+                      <line id="Line_162" data-name="Line 162" class="cls-2" y2="11.176" transform="translate(14.118 5.882)"/>
+                      <line id="Line_163" data-name="Line 163" class="cls-2" y2="11.176" transform="translate(7.059 5.882)"/>
+                      </g>
+                  </g>
+                  </svg>
+              </i>
+              <span>
+                  Delete  <br>Entry
+              </span>
+          </li>
+          <li>
+              <i>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="11900 358 26 22" class="action-icon">
+                    <g id="Group_1235" data-name="Group 1235" transform="translate(10862 -6)">
+                      <rect id="Rectangle_699" data-name="Rectangle 699" class="cls-1" width="26" height="22" transform="translate(1038 364)"/>
+                      <g id="Group_943" data-name="Group 943" transform="translate(-10894.121 -490.387)">
+                      <g id="Group_796" data-name="Group 796" transform="translate(11935.121 855.387)">
+                          <path id="Path_219" data-name="Path 219" class="cls-2" d="M14.084,14.529c-1.647,0-3.588-1.824-4.353-4.412-.647,0-1.176-.765-1.235-1.647a1.765,1.765,0,0,1,.882-1.824C9.555,3.118,10.555,1,14.084,1s4.529,2.118,4.706,5.647c.588.118.941.882.882,1.824s-.588,1.647-1.235,1.647C17.673,12.706,15.731,14.529,14.084,14.529Z" transform="translate(-4.084 -1)"/>
+                          <path id="Path_220" data-name="Path 220" class="cls-2" d="M14.529,20.5C18.294,21.382,21,23.324,21,27.265a25.487,25.487,0,0,1-10,1.765A25.487,25.487,0,0,1,1,27.265c0-3.941,2.706-5.882,6.471-6.765" transform="translate(-1 -9.029)"/>
+                      </g>
+                      <path id="Path_218" data-name="Path 218" class="cls-2" d="M.7,2.181,2.181,3.663,5.144.7" transform="translate(11946.088 868.81)"/>
+                      </g>
+                  </g>
+                  </svg>
+                  </i>
+              <span>
+                  Convert <br> to Student
+              </span>
+          </li>
+          <li>
+              <i>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="11966 358 26 22" class="action-icon">
+                    <g id="Group_1236" data-name="Group 1236" transform="translate(10862 -6)">
+                      <rect id="Rectangle_700" data-name="Rectangle 700" class="cls-1" width="26" height="22" transform="translate(1104 364)"/>
+                      <g id="Group_946" data-name="Group 946" transform="translate(1128 368) rotate(90)">
+                      <g id="Group_1233" data-name="Group 1233">
+                          <path id="Path_239" data-name="Path 239" class="cls-2" d="M17,38.4v-.755s1.277-.83,1.277-1.887V35" transform="translate(-9.191 -21.169)"/>
+                          <path id="Path_240" data-name="Path 240" class="cls-2" d="M4.553,22l-1.7,1.132c-.6.491-.851.755-.851,1.51V30.3" transform="translate(-0.574 -13.075)"/>
+                          <path id="Path_241" data-name="Path 241" class="cls-2" d="M1,47.773V44H8.66v3.773" transform="translate(0 -26.773)"/>
+                          <path id="Path_242" data-name="Path 242" class="cls-2" d="M8,11.944V1h8.936V13.831H10.553" transform="translate(-4.021)"/>
+                          <path id="Path_243" data-name="Path 243" class="cls-2" d="M14.553,14.812h1.7a1.189,1.189,0,0,1,1.277-1.132V6.132A1.189,1.189,0,0,1,16.255,5H13.277A1.189,1.189,0,0,1,12,6.132v5.661" transform="translate(-6.319 -2.49)"/>
+                          <ellipse id="Ellipse_21" data-name="Ellipse 21" class="cls-2" cx="1.489" cy="1.321" rx="1.489" ry="1.321" transform="translate(6.957 5.906)"/>
+                          <path id="Path_244" data-name="Path 244" class="cls-2" d="M6,26.762l3.4-3.019c.766-.679,2,1.019,1.064,2.076L9.4,26.762v1.132A1.534,1.534,0,0,1,7.7,29.4" transform="translate(-2.872 -14.063)"/>
+                      </g>
+                      </g>
+                  </g>
+                  </svg>
+                  </i>
+              <span>
+                 Pay  <br>
+                 Restistration Fees
+              </span>
+          </li>
+      </ul>
+  </div>
+  </div>
+  </div>
+  </div>
+  `,
 })
+
+
 
 export class ActionButtonComponent implements OnInit {
 
-  private active: boolean = false;
-  constructor() { }
-  ngOnInit() { }
+  private showMenu: boolean = false;
+  constructor() {}
+  ngOnInit() {}
+
+  openMenu(ev){
+    this.showMenu = true;
+  }
+
+  closeMenu(){
+    this.showMenu = false;
+  }
 
 }
