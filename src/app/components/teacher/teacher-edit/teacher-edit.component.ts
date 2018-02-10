84b9c795aa8a7ffad6bcc7bfbbea13cc843cc6f3
ProcturDev4 <proctur.dev4@gmail.com>
2018-02-10 11:25:42 +0530

    Changes and bug fixes

--- src/app/components/teacher/teacher-edit/teacher-edit.component.ts
+++ src/app/components/teacher/teacher-edit/teacher-edit.component.ts
@@ -108,13 +108,15 @@ export class TeacherEditComponent implements OnInit {
       this.messageToast('error', 'Error', 'Please provide valid email address.');
       return;
     }
-    if (isNaN(formData.teacher_phone)) {
+    if ((!isNaN(formData.teacher_phone)) || formData.teacher_phone.length != 10) {
       this.messageToast('error', 'Error', 'Please provide valid phone number.');
       return;
     }
-    if ((!isNaN(formData.teacher_alt_phone)) && formData.teacher_alt_phone.length == 10) {
-      this.messageToast('error', 'Error', 'Please provide valid phone number.');
-      return;
+    if (formData.teacher_alt_phone != '' && formData.teacher_alt_phone != null) {
+      if ((!isNaN(formData.teacher_alt_phone)) && formData.teacher_alt_phone.length != 10) {
+        this.messageToast('error', 'Error', 'Please provide valid phone number.');
+        return;
+      }
     }
     if (formData.hour_rate == "" || formData.hour_rate == null) {
       formData.hour_rate = "0";
