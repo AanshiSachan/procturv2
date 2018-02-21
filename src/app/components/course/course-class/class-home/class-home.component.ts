2215750482e7995a9ed597f0d73aa757d49ee830
ProcturDev4 <proctur.dev4@gmail.com>
2018-02-21 18:27:25 +0530

    working copy of class home

--- src/app/components/course/course-class/class-home/class-home.component.ts
+++ src/app/components/course/course-class/class-home/class-home.component.ts
@@ -35,6 +35,16 @@ export class ClassHomeComponent implements OnInit {
     subject_id: '-1',
     teacher_id: '-1',
   }
+  masterCourse: any = [];
+  courseList: any = [];
+  subjectList: any = [];
+  teacherList: any = [];
+  timeTableResponse: any = [];
+  weekScheduleList: any[] = [];
+  showContent: boolean = false;
+  currentDate: Date = new Date();
+  weekStart: any = moment(this.currentDate).isoWeekday("Monday").format("DD MMMM YYYY");
+  weekEnd: any = moment(this.currentDate).isoWeekday("Sunday").format("DD MMMM YYYY");
 
   constructor
     (
@@ -49,15 +59,171 @@ export class ClassHomeComponent implements OnInit {
 
   ngOnInit() {
     this.checkInstituteType();
+    this.getPrefillData();
   }
 
+  getPrefillData() {
+    this.getMasterCourseList()
+    this.getTeachers();
+  }
 
+  getMasterCourseList() {
+    this.classService.getAllMasterCourse().subscribe(
+      res => {
+        this.masterCourse = res;
+        console.log('master', res);
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
 
+  getTeachers() {
+    this.isRippleLoad = true;
+    this.classService.getAllTeachersList().subscribe(
+      res => {
+        console.log('teacher', res);
+        this.isRippleLoad = false;
+        this.teacherList = res;
+      },
+      err => {
+        this.isRippleLoad = false;
+      }
+    )
+  }
 
+  updateCourseList(ev) {
+    this.showContent = false;
+    this.isRippleLoad = true;
+    this.classService.getCourseFromMasterById(ev).subscribe(
+      res => {
+        if (res.coursesList) {
+          console.log("course", res);
+          this.courseList = res;
+          this.isRippleLoad = false;
+        }
+        else {
+          this.courseList = [];
+          this.isRippleLoad = false;
+        }
+      },
+      err => {
+        console.log(err);
+        this.courseList = [];
+        this.isRippleLoad = false;
+      }
+    )
+  }
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  updateSubjectList(event) {
+    this.showContent = false;
+    this.isRippleLoad = true;
+    this.classService.getSubjectList(event).subscribe(
+      res => {
+        this.isRippleLoad = false;
+        console.log('Subject', res);
+        this.subjectList = res.batchesList;
+      },
+      err => {
+        this.isRippleLoad = false;
+        console.log(err);
+      }
+    )
+  }
 
+  getClassList(): any[] {
+    let temp: any[] = [];
+    for (let key in this.timeTableResponse.batchTimeTableList) {
+      let obj = {
+        id: key,
+        data: this.timeTableResponse.batchTimeTableList[key]
+      }
+      temp.push(obj);
+    }
+    console.log(temp);
+    return temp;
+  }
+
+  toggleTbodyClass(i) {
+    debugger
+    //console.log('tbodyItem'+i);
+    document.getElementById('tbodyItem' + i).classList.toggle("active");
+    document.getElementById('tbodyView' + i).classList.toggle("hide");
+    //document.getElementById('tbodyItem'+i).classList.toggle('active');
+  }
+
+  submitMasterCourse() {
+  
+    let data;
+    if (this.isLangInstitute) {
+      data = this.makeJsonForBatch()
+    } else {
+      data = this.makeJsonForSubmit();
+    }
+    this.weekScheduleList = [];
+    this.classService.getTimeTable(data).subscribe(
+      res => {
+        this.messageToast('success', 'Success', 'Success');
+        //console.log(res);
+        this.timeTableResponse = res;
+        this.showContent = true;
+        this.weekScheduleList = this.getClassList();
+        //console.log(this.timeTableResponse);
+      },
+      err => {
+
+      }
+    )
+  }
+
+  makeJsonForSubmit() {
+    let obj: any = {};
+    obj.batch_id = this.getValueOfKey(this.courseList.coursesList, 'batch_id', this.fetchMasterCourseModule.course_id);
+    obj.course_id = this.fetchMasterCourseModule.course_id;
+    obj.master_course = this.courseList.master_course;
+    obj.subject_id = -1;
+    obj.teacher_id = this.fetchMasterCourseModule.teacher_id;
+    obj.standard_id = -1;
+    obj.isExamIncludedInTimeTable = 'Y';
+    obj.startdate = this.getStartDate();
+    obj.enddate = this.getEndDate();
+    obj.type = 2;
+    return obj;
+  }
+
+  makeJsonForBatch() {
+    let obj: any = {};
+    obj.batch_id = null;
+    obj.course_id = this.fetchMasterCourseModule.course_id;
+    obj.master_course = this.courseList.master_course;
+    obj.subject_id = -1;
+    obj.teacher_id = this.fetchMasterCourseModule.teacher_id;
+    obj.standard_id = -1;
+    obj.isExamIncludedInTimeTable = 'Y';
+    obj.startdate = this.getStartDate();
+    obj.enddate = this.getEndDate();
+    obj.type = 2;
+    return obj;
+  }
+
+  getEndDate(): string {
+    let currentDate = moment(this.currentDate).format("YYYY-MM-DD");
+    return moment(currentDate).weekday(7).format("YYYY-MM-DD");
+  }
+
+  getStartDate(): string {
+    let currentDate = moment(this.currentDate).format("YYYY-MM-DD");
+    return moment(currentDate).weekday(1).format("YYYY-MM-DD");
+  }
+
+  getValueOfKey(data, key, value) {
+    for (let t = 0; t < data.length; t++) {
+      if (data[t][key] == value) {
+        return data[t].batch_id;
+      }
+    }
+  }
 
   messageToast(errorType, errorTitle, errorMeassage) {
     let data = {
@@ -74,6 +240,47 @@ export class ClassHomeComponent implements OnInit {
       this.isLangInstitute = true;
     } else {
       this.isLangInstitute = false;
+    }
+  }
+
+  gotoPreviousWeek() {
+    this.currentDate = new Date(moment(this.currentDate).subtract(7, 'd').format("YYYY-MM-DD"));
+    this.weekStart = moment(this.currentDate).isoWeekday("Monday").format("DD MMMM YYYY");
+    this.weekEnd = moment(this.currentDate).isoWeekday("Sunday").format("DD MMMM YYYY");
+    this.submitMasterCourse();
+  }
+  /* ============================================================================================ */
+  /* ============================================================================================ */
+  gotoNextWeek() {
+    this.currentDate = new Date(moment(this.currentDate).add(7, 'd').format("YYYY-MM-DD"));
+    this.weekStart = moment(this.currentDate).isoWeekday("Monday").format("DD MMMM YYYY");
+    this.weekEnd = moment(this.currentDate).isoWeekday("Sunday").format("DD MMMM YYYY");
+    this.submitMasterCourse();
+  }
+
+  delete(level, index, subIndex){
+    if(level == 'course'){
+      console.log(this.weekScheduleList[index]);
+      console.log('this has to be deleted');
+    }
+    else if(level == 'subject'){
+      console.log(this.weekScheduleList[index].data[subIndex]);
+      console.log('this has to be deleted');
+    }
+    else if(level == 'batch'){}
+  }
+
+  notify(level, index, subIndex){
+    if(level == 'course'){
+      console.log(this.weekScheduleList[index]);
+      console.log('this has to be notified');
+    }
+    else if(level == 'subject'){
+      console.log(this.weekScheduleList[index].data[subIndex]);
+      console.log('this has to be deleted');
+    }
+    else if(level == 'batch'){
+
     }
   }
 
