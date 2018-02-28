a8b7b1ea452b5660ee7a2cb0fbf2d99a612b49dc
ProcturDev4 <proctur.dev4@gmail.com>
2018-02-28 11:20:11 +0530

    error resolving

--- src/app/services/authenticator.service.ts
+++ src/app/services/authenticator.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map'
- 
+
 /* Method declared for future purpose for setting authorization after successfull login */
 
 @Injectable()
@@ -11,41 +11,40 @@ export class AuthenticatorService {
     public institute_id: any = null;
     public institute_type: any = null;
     public institution_type: any = null;
-    public baseUrl:string = "http://test999.proctur.com/StdMgmtWebAPI"
+    public baseUrl: string = "http://test999.proctur.com/StdMgmtWebAPI"
 
 
     constructor() {
-        // set token if saved in session storage
-        this.institute_id = sessionStorage.getItem('institute_id');
-        this.token = sessionStorage.getItem('Authorization');
-        this.institute_type = sessionStorage.getItem('institute_type');
     }
 
-    getAuthToken(){
-        if(this.token != null){
+    getAuthToken() {
+        this.token = sessionStorage.getItem('Authorization');
+        if (this.token != null) {
             return this.token;
         }
-        else{
+        else {
         }
     }
 
-    getInstituteType(){
-        if(this.institute_type != null){
+    getInstituteType() {
+        this.institute_type = sessionStorage.getItem('institute_type');
+        if (this.institute_type != null) {
             return this.institute_type;
         }
-        else{
+        else {
         }
     }
 
-    getInstituteId(){
-        if(this.institute_id != null){
+    getInstituteId() {
+        this.institute_id = sessionStorage.getItem('institute_id');
+        if (this.institute_id != null && this.institute_id != undefined) {
             return this.institute_id;
         }
-        else{
+        else {
         }
     }
 
-    getBaseUrl(): string{
+    getBaseUrl(): string {
         return this.baseUrl;
     }
 
