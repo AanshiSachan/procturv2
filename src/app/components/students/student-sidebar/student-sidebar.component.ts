9abd32d9a17389b6be751ae727ade14f5a6e0ff1
ProcturDev4 <35286687+ProcturDev4@users.noreply.github.com>
2018-03-22 18:01:57 +0530

    leave popup changes done

--- src/app/components/students/student-sidebar/student-sidebar.component.ts
+++ src/app/components/students/student-sidebar/student-sidebar.component.ts
@@ -17,6 +17,7 @@ export class StudentSidebarComponent implements OnInit, OnChanges {
   @Output() editStudent = new EventEmitter<any>();
   @Output() deleteStudent = new EventEmitter<any>();
   @Output() editNotes = new EventEmitter<any>();
+  @Output() leaveEvent = new EventEmitter<any>();
 
   //@ViewChild('acc') acc: ElementRef;
   @ViewChild('one') one: ElementRef;
@@ -65,6 +66,10 @@ export class StudentSidebarComponent implements OnInit, OnChanges {
 
   emitDelete() {
     this.deleteStudent.emit(this.rowData);
+  }
+
+  emitLeave() {
+    this.leaveEvent.emit(this.rowData.student_id);
   }
 
   emitNotes() {
