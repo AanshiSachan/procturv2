f30245089ae24877ef1db15b802004b7729a18ad
procturdev8 <proctur.dev8@gmail.com>
2018-03-28 14:22:33 +0530

    style admin attendance

--- src/app/components/homepage-dashboard/admin-home/admin-home.component.ts
+++ src/app/components/homepage-dashboard/admin-home/admin-home.component.ts
@@ -2231,20 +2231,19 @@ export class AdminHomeComponent implements OnInit {
 
 
   //  Role Based Access
-
   checkIfUserHadAccess(id) {
-    if (sessionStorage.getItem('permissions') == '') {
-      return true;
-    }
-    else {
-      if (JSON.parse(sessionStorage.getItem('permissions')).includes(id)) {
+    if (this.permissionArray == "") {
+      return false;
+    } else {
+      let data = JSON.parse(this.permissionArray);
+      if (data.indexOf(id) == "-1") {
         return true;
-      }
-      else {
+      } else {
         return false;
       }
     }
   }
+
 
   markAttendaceBtnClickCourse(event, rowData, index) {
     document.getElementById('leaveBtnCourse' + rowData.student_id).classList.remove('classLeaveBtn');
