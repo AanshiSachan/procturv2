49b897ae72a576cbef4fa0844860444a0f095c18
procturdev8 <proctur.dev8@gmail.com>
2018-04-02 18:26:58 +0530

    changes in email

--- src/app/components/report/email-report/email-report.component.ts
+++ src/app/components/report/email-report/email-report.component.ts
@@ -1,21 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AppComponent } from '../../../app.component';
+import { LoginService } from '../../../services/login-services/login.service';
+import { document } from '../../../../assets/imported_modules/ngx-bootstrap/utils/facade/browser';
+import { ColumnSetting } from '../../shared/custom-table/layout.model';
+import { getEmailService } from '../../../services/report-services/get-email.service';
+import * as moment from 'moment';
 @Component({
   selector: 'app-email-report',
   templateUrl: './email-report.component.html',
   styleUrls: ['./email-report.component.scss']
 })
 export class EmailReportComponent implements OnInit {
+  email_data: any;
 
-  constructor() {
+  isProfessional: boolean = false;
+  smsSource: any[] = [];
+  
+
+
+
+  projectSettings: ColumnSetting[] = [
+    { primaryKey: 'em_name', header: 'Name' },
+    { primaryKey: 'em_phone', header: 'Contact No.' },
+    { primaryKey: 'em_Email', header: 'Email' },
+    { primaryKey: 'em_sentDateTime', header: 'Sent Date' },
+    { primaryKey: 'em_Email_type', header: 'Type' },
+    { primaryKey: 'em_func_type', header: 'Event' },
+    { primaryKey: 'em_sentStatus', header: 'Name' }
+  ];
+                  
+
+  emailFetchForm: any = {
+    institution_id: parseInt(sessionStorage.getItem('institute_id')),
+    from_date: moment().format('YYYY-MM-DD'),
+    to_date: moment().format('YYYY-MM-DD')
+  }
+
+  constructor(
+    private fetchApiService: getEmailService,
+    private login: LoginService) {
     this.switchActiveView('email');
-   }
+  }
 
   ngOnInit() {
+    this.getAllEmailMessages();
+  }
+
+  getAllEmailMessages() {
+    this.fetchApiService.getEmailMessages(this.emailFetchForm).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
 
-  switchActiveView(id){
+
+switchActiveView(id) {
     document.getElementById('home').classList.remove('active');
     document.getElementById('attendance').classList.remove('active');
     document.getElementById('sms').classList.remove('active');
@@ -25,18 +70,5 @@ export class EmailReportComponent implements OnInit {
     document.getElementById('time').classList.remove('active');
     document.getElementById('email').classList.remove('active');
     document.getElementById('profit').classList.remove('active');
-    switch(id){
-      case 'home': { document.getElementById('home').classList.add('active'); break; }
-      case 'attendance': { document.getElementById('attendance').classList.add('active'); break; }
-      case 'sms': { document.getElementById('sms').classList.add('active'); break; }
-      case 'fee': { document.getElementById('fee').classList.add('active'); break; }
-      case 'exam': { document.getElementById('exam').classList.add('active'); break; }
-      case 'report': { document.getElementById('report').classList.add('active'); break; }
-      case 'time': { document.getElementById('time').classList.add('active'); break; }
-      case 'email': { document.getElementById('email').classList.add('active');  break; }
-      case 'profit': { document.getElementById('profit').classList.add('active'); break; }
-    }
-  }
-
-
-}
+    document.getElementById('email').classList.add('active');
+  }}
---
+++ src/app/services/report-services/get-email.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import * as moment from 'moment';
+import { AuthenticatorService } from "../authenticator.service";
+
+@Injectable()
+export class getEmailService {
+    baseUrl: string = '';
+    institute_id: string;
+    Authorization: string;
+    headers: HttpHeaders;
+
+
+    /* set default value for each url, header and autherization on service creation */
+    constructor(private http: HttpClient, private auth1: AuthenticatorService, ) {
+        this.Authorization = sessionStorage.getItem('Authorization');
+        this.institute_id = sessionStorage.getItem('institute_id');
+        this.baseUrl = this.auth1.getBaseUrl();
+        this.headers = new HttpHeaders({ "Content-Type": "application/json", "Authorization": this.Authorization });
+    }
+
+
+    getEmailMessages(obj): Observable<any> {
+
+        obj.from_date = obj.from_date == '' ? moment().format('YYYY-MM-DD') : moment(obj.from_date).format('YYYY-MM-DD');
+        obj.to_date = obj.to_date == '' ? '' : moment(obj.to_date).format('YYYY-MM-DD');
+
+
+        let url = this.baseUrl + "/alerts/config/emailReport";
+        return this.http.post(url, obj, { headers: this.headers }).map(
+            res => {
+                return res;
+            },
+            err => {
+                return err;
+            }
+        )
+    }
+
+
+
+
+    /*    fetchSmsReport(obj) {
+    
+            obj.from_date = obj.from_date == '' ? moment().format('YYYY-MM-DD') : moment(obj.from_date).format('YYYY-MM-DD');
+            obj.to_date = obj.to_date == '' ? '' : moment(obj.to_date).format('YYYY-MM-DD');
+    
+            let urlSmsReport = this.baseUrl + "/api/v1/alerts/config/smsReport";
+    
+            return this.http.post(urlSmsReport, obj, { headers: this.headers }).map(
+                res => {
+                    return res.json();
+                },
+                err => {
+                    return err.json();
+                }
+            )
+    
+        }
+    */
+
+
+
+}
