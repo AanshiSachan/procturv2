d7bb6835f99cee9d43c01ce4ade96738bfd1acd9
procturdev8 <proctur.dev8@gmail.com>
2018-04-09 18:37:39 +0530

    email_repo

--- src/app/components/report/email-report/email-report.component.ts
+++ src/app/components/report/email-report/email-report.component.ts
@@ -1,44 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AppComponent } from '../../../app.component';
 import { LoginService } from '../../../services/login-services/login.service';
-import { document } from '../../../../assets/imported_modules/ngx-bootstrap/utils/facade/browser';
 import { ColumnSetting } from '../../shared/custom-table/layout.model';
 import { getEmailService } from '../../../services/report-services/get-email.service';
 import * as moment from 'moment';
+
 @Component({
   selector: 'app-email-report',
   templateUrl: './email-report.component.html',
   styleUrls: ['./email-report.component.scss']
 })
-export class EmailReportComponent {
-  email_data: any;
-  email_fromDate:string= "";
-  email_toDate:string= "";
 
-  isProfessional: boolean = false;
+export class EmailReportComponent {
+  pageIndex: number = 1;
+  totalRecords: number = 0;
+  displayBatchSize: number = 10;
   emailSource: any[] = [];
-  
+  emailDataSource: any = [];
+
   projectSettings: ColumnSetting[] = [
     { primaryKey: 'sentDateTime', header: 'Sent Date' },
     { primaryKey: 'emailId', header: 'Email' },
-    { primaryKey: 'subject', header: 'Subject' },
+    { primaryKey: 'message', header: 'Subject' },
     { primaryKey: 'name', header: 'Name' },
     { primaryKey: 'role', header: 'Role' },
     { primaryKey: 'sms_type', header: 'Type' }
   ];
-                  
-   
-    emailFetchForm: any = {
-    institution_id: parseInt(sessionStorage.getItem('institute_id')),
-    from_date: moment('01 March 2018').format('YYYY-MM-DD'),
-    to_date: moment('30 March 2018').format('YYYY-MM-DD')
 
+
+  emailFetchForm: any = {
+    institution_id: parseInt(sessionStorage.getItem('institute_id')),
+    from_date: '',
+    to_date: '',
   }
 
   constructor(
-    private fetchApiService: getEmailService, 
-    private login: LoginService ,private appC :AppComponent) {
+    private apiService: getEmailService,
+    private login: LoginService,
+    private appC: AppComponent,
+  ) {
     this.switchActiveView('email');
   }
 
@@ -47,10 +47,13 @@ export class EmailReportComponent {
   }
 
   getAllEmailMessages() {
+    this.pageIndex = 1;
     this.emailSource = [];
-    this.fetchApiService.getEmailMessages(this.emailFetchForm).subscribe(
+    this.apiService.getEmailMessages(this.emailFetchForm).subscribe(
       res => {
-        this.emailSource = res;
+        this.emailDataSource = res;
+        this.totalRecords = res.length;
+        this.fetchTableDataByPage(this.pageIndex);
         console.log(res);
       },
       err => {
@@ -59,48 +62,88 @@ export class EmailReportComponent {
     )
   }
 
-  isTimeValid():boolean{
-   let v= moment(this.email_fromDate).diff(moment(this.email_toDate))
-   
-    if(v<=0){
+
+  isTimeValid(): boolean {
+    let v = moment(this.emailFetchForm.from_date).diff(moment(this.emailFetchForm.to_date))
+    if (v <= 0) {
       return true;
     }
-  else{
-    return false;
-  }
-  }
- fetchemailByDate(){
-    if(this.isTimeValid()){
-      let email_value={
-       institution_id : parseInt(sessionStorage.getItem('institute_id')),
-       from_date: moment(this.email_fromDate).format('YYYY-MM-DD'),
-       to_date: moment(this.email_toDate).format('YYYY-MM-DD')
-
-      }
-       this.fetchApiService.getEmailMessages(email_value)
-       .subscribe(store=>{this.emailSource=store;console.log(store)},err=>{console.log(err)})
- 
+    else {
+      return false;
     }
-    else{
-      let obj={
-        type:"error",
-        title:"Invalid Date Range Selected",
+  }
+
+
+  fetchemailByDate() {
+    if (this.isTimeValid()) {
+      this.getAllEmailMessages();
+    }
+    else {
+      let obj = {
+        type: "error",
+        title: "Invalid Date Range Selected",
         Body: "From date cannot be greater than To date"
-       }
-   this.appC.popToast(obj);
+      }
+      this.appC.popToast(obj);
 
     }
   }
 
-switchActiveView(id) {
-    document.getElementById('home').classList.remove('active');
-    document.getElementById('attendance').classList.remove('active');
-    document.getElementById('sms').classList.remove('active');
-    document.getElementById('fee').classList.remove('active');
-    document.getElementById('exam').classList.remove('active');
-    document.getElementById('report').classList.remove('active');
-    document.getElementById('time').classList.remove('active');
+  // getEmailRepo(obj) {
+  //   if (obj.start_index == 0) {
+  //     return this.apiService.getEmailMessages(obj).subscribe(
+  //       res => {
+  //         if (res.length != 0) {
+  //           this.smsSource = res;
+  //           this.totalRecords = res[0].totalRecords;
+
+  //         }
+  //         else {
+  //           this.smsSource = [];
+  //           this.totalRecords = 0;
+  //         }
+  //       }
+  //     )
+
+  //   }
+
+  //   else {
+  //     return this.apiService.getEmailMessages(obj).subscribe(
+  //       res => {
+  //         this.smsSource = res;
+  //       }
+  //     )
+  //   }
+  // }
+
+  // pagination functions 
+
+  fetchTableDataByPage(index) {
+    this.pageIndex = index;
+    let startindex = this.displayBatchSize * (index - 1);
+    this.emailSource = this.getDataFromDataSource(startindex);
+  }
+
+  fetchNext() {
+    this.pageIndex++;
+    this.fetchTableDataByPage(this.pageIndex);
+  }
+
+  fetchPrevious() {
+    if (this.pageIndex != 1) {
+      this.pageIndex--;
+      this.fetchTableDataByPage(this.pageIndex);
+    }
+  }
+
+  getDataFromDataSource(startindex) {
+    let t = this.emailDataSource.slice(startindex, startindex + this.displayBatchSize);
+    return t;
+  }
+
+
+  switchActiveView(id) {
     document.getElementById('email').classList.remove('active');
-    document.getElementById('profit').classList.remove('active');
-    document.getElementById('email').classList.add('active');
-  }}
+  }
+
+}
