a52652e28197ba2b7536e4f42af35a830209de8d
ronit123321 <ronit.oommen@gmail.com>
2018-04-12 21:25:56 +0530

    fixes

--- src/app/components/students/student-sidebar/student-sidebar.component.ts
+++ src/app/components/students/student-sidebar/student-sidebar.component.ts
@@ -59,6 +59,7 @@ export class StudentSidebarComponent implements OnInit, OnChanges {
     this.rowData;
     this.studentDetails;
     this.customComponent;
+    console.log(this.studentDetails);
     this.fetchStudentDetails(this.studentDetails);
   }
 
