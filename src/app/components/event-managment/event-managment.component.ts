e1d01dffb626c58b404d78c05fc6abfc31ad7c1e
procturdev8 <proctur.dev8@gmail.com>
2018-05-03 14:07:59 +0530

    classroom

--- src/app/components/event-managment/event-managment.component.ts
+++ src/app/components/event-managment/event-managment.component.ts
@@ -10,11 +10,15 @@ import { AppComponent } from '../../app.component';
 export class EventManagmentComponent implements OnInit {
   isProfessional: boolean = false;
   eventRecord: any = [];
+  endDatecheckbox:boolean= false;
   endDateBox: boolean = false;
   getHoliday: any = [];
+  checker:boolean=false;
   getEvent: any = [];
+  endDateofEvent:boolean=false;
   closeEditPopup: boolean = false;
   totalRow = 0;
+  generalUpdateDataField:boolean=false;
   generalDataField: boolean = false;
   pagedSourceData: any[] = [];
   pageIndex: number = 1;
@@ -53,6 +57,7 @@ export class EventManagmentComponent implements OnInit {
     holiday_date: moment().format("YYYY-MM-DD"),
     holiday_desc: "",
     holiday_long_desc: "",
+    holidayId:"",
     holiday_name: "",
     holiday_type: "",
     image: null,
@@ -68,7 +73,8 @@ export class EventManagmentComponent implements OnInit {
     this.getAllListData();
     this.isProfessional = sessionStorage.getItem('institute_type') == 'LANG';
   }
-
+/*=====================================get list of records=================================
+============================================================================================ */
   getAllListData() {
 
     this.eve_mnge.getListEventDesc(this.list_obj).subscribe(
@@ -83,7 +89,7 @@ export class EventManagmentComponent implements OnInit {
       }
     )
   }
-
+/*
   isTimeValid(): boolean {
     let v = moment(this.saveDataObj.holiday_date).diff(moment(this.saveDataObj.event_end_date))
     if (v <= 0) {
@@ -110,7 +116,9 @@ export class EventManagmentComponent implements OnInit {
 
     }
   }
-
+*/
+/*================================================get events==============================
+============================================================================================= */
   getEvents() {
     this.eve_mnge.getEventdata().subscribe(
       res => {
@@ -123,15 +131,8 @@ export class EventManagmentComponent implements OnInit {
       }
     )
   }
-
-  eventchange(para) {
-    if (this.saveDataObj.event_type == "2") {
-      this.generalDataField = true;
-    }
-    else {
-      this.generalDataField = false;
-    }
-  }
+   /*=====================================get holidays============================================
+  =============================================================================================== */
   getHolidays() {
     this.eve_mnge.getHolidayData().subscribe(
       res => {
@@ -143,7 +144,26 @@ export class EventManagmentComponent implements OnInit {
       }
     )
   }
-
+/*==============================================validate event=============================
+========================================================================================== */
+  eventchange(para) {
+    if (this.saveDataObj.event_type == "2") {
+      this.generalDataField = true;
+    }
+    else {
+      this.generalDataField = false;
+    }
+  }
+  /*========================================validate event at updatetime=======================
+  ==============================================================================================  */
+  eventchangeUpdate(para){
+if(this.newUpdateObj.event_type=="2"){
+  this.generalUpdateDataField=true;
+}
+else{
+  this.generalUpdateDataField=false;
+}
+  }
   saveEventData() {
 
     this.eve_mnge.saveEventDescData(this.saveDataObj).subscribe(
@@ -157,13 +177,17 @@ export class EventManagmentComponent implements OnInit {
       }
     )
   }
+  /*==========================================get update data================================
+  ========================================================================================== */
   updatePopupData() {
-    console.log(this.updateListObj);
-
+  
+    this.newUpdateObj.event_end_date = moment(this.newUpdateObj.event_end_date).format('YYYY-MM-DD');
+    this.newUpdateObj.holiday_date = moment(this.newUpdateObj.holiday_date).format('YYYY-MM-DD');
     this.eve_mnge.getUpdateEventData(this.newUpdateObj).subscribe(
       res => {
         console.log(res);
         this.closeEditPopup = false;
+        this.getAllListData();
       },
       error => {
         console.log(error);
@@ -174,12 +198,19 @@ export class EventManagmentComponent implements OnInit {
 
   checkChange(para) {
     if (para == true) {
-      this.endDateBox = true;
+      this.endDatecheckbox=true;
+      this.endDateBox=true;
+      this.endDateofEvent=true;
+      
     }
     else {
-      this.endDateBox = false;
+      this.endDatecheckbox = false;
+      this.endDateBox=false;
+      this.endDateofEvent=false;
     }
   }
+  /*=============================================edit update=========================
+  ===================================================================================== */
 
   updateEventForm(holidayId) {
     this.eve_mnge.updateEventData(holidayId).subscribe(
@@ -187,14 +218,22 @@ export class EventManagmentComponent implements OnInit {
         console.log(res);
         this.updateListObj = res;
         this.newUpdateObj.event_type = res.event_type;
-        this.newUpdateObj.holiday_date = res.holiday_date;
+        this.newUpdateObj.holiday_date = moment(res.holiday_date).format("YYYY-MM-DD");
         this.newUpdateObj.holiday_desc=res.holiday_desc;
         this.newUpdateObj.holiday_long_desc=res.holiday_long_desc;
         this.newUpdateObj.holiday_name= res.holiday_name;
         this.newUpdateObj.holiday_type= res.holiday_type;
-        this.newUpdateObj.event_end_date= res.event_end_date;
+        this.newUpdateObj.holidayId= res.holidayId;
+        this.newUpdateObj.event_end_date= moment(res.event_end_date).format("YYYY-MM-DD");
         this.newUpdateObj.image= res.image;
         this.newUpdateObj.public_url= res.public_url;
+        if(res.event_end_date==""){
+          this.checker=false;
+
+        }
+        else{
+          this.checker=true;
+        }
       console.log(this.newUpdateObj);  
       },
       
@@ -203,32 +242,36 @@ export class EventManagmentComponent implements OnInit {
       }
     )
   }
-
+/*===================================================delete event data========================
+============================================================================================== */
   deleteEventDataFromList(holidayId) {
     this.eve_mnge.deleteEventData(holidayId).subscribe(
       res => {
         console.log(res);
-
+        this.getAllListData();
       },
       error => {
         console.log(error);
       }
     )
   }
+/*==================================send notification to EventType="GENERAL"======================
+============================================================================================== */
 
-
-  sendNotificationAlert() {
-    if (this.list_obj.event_type == "2") {
-      alert("are u sure u want to send notification ");
-
-      this.eve_mnge.sendNotifiation(this.sendNotify_obj).subscribe(
+  sendNotificationAlert(e) {
+     var prompt =  confirm("Are you sure,you want to Send Push Notification?");
+    if(prompt){
+        this.sendNotify_obj.event_id= e;
+       this.eve_mnge.sendNotifiation(this.sendNotify_obj).subscribe(
         res => {
-          console.log(res);
+
         },
         error => {
-          console.log(error);
-        }
+      }
       )
+    }
+    else{
+
     }
   }
 
@@ -246,7 +289,14 @@ export class EventManagmentComponent implements OnInit {
 
   }
   deleteEntryData(holidayId) {
-   // this.deleteEventData(holidayId);
+    var prompt =  confirm("Are you sure, you want to delete the Event?");
+    if(prompt){
+      this.deleteEventDataFromList(holidayId);
+    }
+    else{
+      
+    }
+  
   }
   closeReportPopup() {
     this.closeVarPopup = false;
@@ -255,7 +305,8 @@ export class EventManagmentComponent implements OnInit {
   closeUpdateReportPopup() {
     this.closeEditPopup = false;
   }
-  /*====================================================pagination */
+  /*====================================================pagination========================
+  ======================================================================================== */
   fetchTableDataByPage(index) {
     this.pageIndex = index;
     let startindex = this.displayBatchSize * (index - 1);
