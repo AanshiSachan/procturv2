9fa1d97a10a3abad7d4d13846d19ba96af4a1056
proctur.dev4@ <proctur.dev4@gmail.com>
2018-05-18 12:22:54 +0530

    dashboard issue fix

--- src/app/components/homepage-dashboard/admin-home/admin-home.component.ts
+++ src/app/components/homepage-dashboard/admin-home/admin-home.component.ts
@@ -1319,7 +1319,7 @@ export class AdminHomeComponent implements OnInit {
         }
         this.courseLevelSchedule = tempArr;
         this.isRippleLoad = false;
-        this.isSubjectView = false;
+        // this.isSubjectView = false;
       },
       err => {
         console.log(err);
