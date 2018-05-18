46ccef34c0071481af0f869522f43c62487cb435
ronit123321 <ronit.oommen@gmail.com>
2018-05-18 17:47:34 +0530

    custom table update

--- src/app/components/activity-home/fee-activity/cheque-manage/cheque-manage.component.ts
+++ src/app/components/activity-home/fee-activity/cheque-manage/cheque-manage.component.ts
@@ -5,6 +5,7 @@ import * as moment from 'moment';
 import { getCheque } from '../../../../services/cheque-manage/get-cheque.service';
 import { ColumnData } from '../../../shared/ng-robAdvanceTable/ng-robAdvanceTable.model';
 import { DropData } from '../../../shared/ng-robAdvanceTable/dropmenu/dropmenu.model';
+import { copyConfig } from '@angular/router/src/config';
 
 
 @Component({
@@ -14,6 +15,9 @@ import { DropData } from '../../../shared/ng-robAdvanceTable/dropmenu/dropmenu.m
 })
 export class ChequeManageComponent implements OnInit {
 
+  pdcDetails: any;
+  studentFeeDues: any[];
+  isPendingUpdate: boolean;
   isUpdatePopup: boolean;
   actionSelected: any;
   selectedRecord: any;
@@ -27,7 +31,7 @@ export class ChequeManageComponent implements OnInit {
     contact_no: '',
   }
 
-  dropType:number = 1;
+  dropType: number = 1;
 
   searchValue: any = ''
 
@@ -46,7 +50,11 @@ export class ChequeManageComponent implements OnInit {
 
   menuList: DropData[] = [
     { key: 'update', header: 'Update' }
+    /* { key: 'pending', header: 'Update Payment' } */
   ];
+
+  chequeUpdateStatus: any;
+
 
   constructor(private login: LoginService, private appC: AppComponent, private getter: getCheque) {
     this.dateRange[0] = new Date(moment().date(1).format("YYYY-MM-DD"));
@@ -63,13 +71,12 @@ export class ChequeManageComponent implements OnInit {
     this.getter.getChequeTypes(obj).subscribe(
       res => {
         this.chequeDataSource = res;
-        if(res == null || res.length == 0){
-          this.dataStatus = 0;          
+        if (res == null || res.length == 0) {
+          this.dataStatus = 0;
         }
       },
       err => {
         this.dataStatus = 0;
-        console.log(err);
       }
     );
   }
@@ -84,10 +91,10 @@ export class ChequeManageComponent implements OnInit {
       contact_no: '',
     }
 
-    if(isNaN(this.searchValue)){
+    if (isNaN(this.searchValue)) {
       obj.student_name = this.searchValue;
     }
-    else{
+    else {
       obj.contact_no = this.searchValue;
     }
 
@@ -95,16 +102,126 @@ export class ChequeManageComponent implements OnInit {
 
   }
 
-  optionSelected(e){
-      console.log(e);
-      this.selectedRecord = e.data;
-      this.actionSelected = e.action._value;
-      this.isUpdatePopup = true;
+  optionSelected(e) {
+    this.selectedRecord = e.data;
+    this.actionSelected = e.action._value;
+    this.decidePopup(e.data);
   }
 
-  cancelUpdate(){
+  cancelUpdate() {
     this.isUpdatePopup = false;
+    this.isPendingUpdate = false;
   }
+
+  decidePopup(d) {
+    if (d.cheque_status_id == 3) {
+      this.chequeUpdateStatus = "3"
+      this.isUpdatePopup = true;
+    }
+    else if (d.cheque_status_id == 1) {
+      this.fetchChequePaymentData();
+      this.isPendingUpdate = true;
+    }
+  }
+
+
+  fetchChequePaymentData() {
+    this.getStudentFeeDetails();
+    this.getAllChequeStudent();
+    this.getPdcDetails();
+  }
+
+  getStudentFeeDetails(){
+    this.getter.fetchStudentFeeDetails(this.selectedRecord.student_id).subscribe(
+      res => {
+        if(res.studentFeeReportJsonList != null){
+          for(let k in res.studentFeeReportJsonList){
+            res.studentFeeReportJsonList[k].toPay = '';
+            res.studentFeeReportJsonList[k].balanceDueOn = res.studentFeeReportJsonList[k].due_date;
+            res.studentFeeReportJsonList[k].selected = false;
+          }
+          this.studentFeeDues = res;
+        }
+      },
+      err => {}
+    )
+  }
+
+  getPdcDetails(){
+    console.log(this.selectedRecord);
+    this.getter.fetchPdcChequeDetails(this.selectedRecord.pdc_cheque_id).subscribe(
+      res => {
+        this.pdcDetails = res;
+      },
+      err => {}
+    )
+  }
+
+  getAllChequeStudent(){
+    this.getter.fetchAllChequeStudent(this.selectedRecord.student_id).subscribe(
+      res => {
+        this.studentFeeDues = res;
+      },
+      err => {}
+    )
+  }
+
+  updateRecord() {
+
+    let obj = {
+      student_id: this.selectedRecord.student_id,
+      payment_reference_id: this.selectedRecord.payment_reference_id,
+      cheque_status_id: this.chequeUpdateStatus,
+      cheque_id: this.selectedRecord.cheque_id,
+      financial_year: this.selectedRecord.financial_year
+    }
+
+    this.getter.updateChequeStatus(obj).subscribe(
+      res => {
+        this.updateRecordOnClient();
+        let msg = {
+          type: "success",
+          title: "Cheque Status Updated",
+          body: ""
+        }
+        this.appC.popToast(msg);
+        this.isUpdatePopup = false;
+      },
+      err => {
+        let msg = {
+          type: "error",
+          title: "An Error Occured",
+          body: "Please contact support@proctur.com"
+        }
+        this.appC.popToast(msg);
+      }
+    )
+
+  }
+
+  updateRecordOnClient() {
+    let temp: any[] = this.chequeDataSource.map(e => {
+      if (e.cheque_id == this.selectedRecord.cheque_id) {
+        e.cheque_status_id = this.chequeUpdateStatus;
+        if (e.cheque_status_id == 2) {
+          e.cheque_status = "dishonoured"
+        }
+        else if (e.cheque_status_id == 3) {
+          e.cheque_status = "cleared";
+        }
+
+        return e;
+      }
+      else {
+        return e;
+      }
+    });
+
+    this.selectedRecord = null;
+    this.chequeUpdateStatus = "3";
+    this.chequeDataSource = temp;
+  }
+
 
   
 
