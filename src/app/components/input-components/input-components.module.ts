772a92b551cb621d5643f2674825acac32743c7c
ronit123321 <ronit.oommen@gmail.com>
2018-05-29 18:52:24 +0530

    custom inputs

---
+++ src/app/components/input-components/input-components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    declarations: [
+
+    ],
+    exports: [
+        CommonModule,
+
+    ],
+    entryComponents: [
+    ],
+    providers: [
+
+    ]
+})
+export class InputComponentModule { }
