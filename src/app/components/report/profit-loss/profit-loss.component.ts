455258cd91ed1470c028d42a770f1e50b92aed53
ankits1183531 <ankits1183531@gmail.com>
2018-05-29 18:51:54 +0530

    profitnloss

--- src/app/components/report/profit-loss/profit-loss.component.ts
+++ src/app/components/report/profit-loss/profit-loss.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfitLossServiceService } from '../../../services/profit-loss-service/profit-loss-service.service';
+import { AppComponent } from '../../../app.component';
+import { AuthenticatorService } from '../../../services/authenticator.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-profit-loss',
@@ -7,36 +11,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfitLossComponent implements OnInit {
 
-  constructor() { 
-    this.switchActiveView('profit');
+  getExpenses ={
+    enddate:moment().format('YYYY-MM-DD'),
+    startdate:moment().format('YYYY-MM-DD'),
+    institute_id:this.profitLoss.institute_id,
+    type:0
   }
+  isRippleLoad: boolean = false;
+
+  constructor(private profitLoss: ProfitLossServiceService,
+    private appc: AppComponent,
+    private institute_id: AuthenticatorService) {
+  }
+
 
   ngOnInit() {
+    this.fetchAllData();
   }
 
 
-  switchActiveView(id){
-    document.getElementById('home').classList.remove('active');
-    document.getElementById('attendance').classList.remove('active');
-    document.getElementById('sms').classList.remove('active');
-    document.getElementById('fee').classList.remove('active');
-    document.getElementById('exam').classList.remove('active');
-    document.getElementById('report').classList.remove('active');
-    document.getElementById('time').classList.remove('active');
-    document.getElementById('email').classList.remove('active');
-    document.getElementById('profit').classList.remove('active');
-    switch(id){
-      case 'home': { document.getElementById('home').classList.add('active'); break; }
-      case 'attendance': { document.getElementById('attendance').classList.add('active'); break; }
-      case 'sms': { document.getElementById('sms').classList.add('active'); break; }
-      case 'fee': { document.getElementById('fee').classList.add('active'); break; }
-      case 'exam': { document.getElementById('exam').classList.add('active'); break; }
-      case 'report': { document.getElementById('report').classList.add('active'); break; }
-      case 'time': { document.getElementById('time').classList.add('active'); break; }
-      case 'email': { document.getElementById('email').classList.add('active');  break; }
-      case 'profit': { document.getElementById('profit').classList.add('active'); break; }
-    }
+  fetchAllData() {
+    this.fetchexpenseData();
+    this.fetchIncomeFees();
+    this.fetchProfitLoss();
+    this.fetchIncome();
   }
 
+
+
+  fetchexpenseData() {
+
+    this.isRippleLoad = true;
+    this.profitLoss.fetchExpenseDetails(this.getExpenses).subscribe(
+      (data: any) => {
+        this.isRippleLoad = false;
+      },
+      (error: any) => {
+        this.isRippleLoad = false;
+        let msg = {
+          type: "error",
+          body: error.error.message
+        }
+        this.appc.popToast(msg);
+        return error;
+      }
+    )
+  }
+
+
+
+  fetchIncomeFees() {
+    
+    this.profitLoss.fetchIncomeFeesCollection(this.getExpenses).subscribe(
+      (data: any) => {
+        this.isRippleLoad = false;
+      },
+      (error: any) => {
+        this.isRippleLoad = false;
+        let msg = {
+          type: "error",
+          body: error.error.message
+        }
+        this.appc.popToast(msg);
+        return error;
+      }
+    )
+  }
+
+
+
+  fetchProfitLoss() {
+    
+    this.profitLoss.fetchProfitLossReport(this.getExpenses).subscribe(
+      (data: any) => {
+        this.isRippleLoad = false;
+      },
+      (error: any) => {
+        this.isRippleLoad = false;
+        let msg = {
+          type: "error",
+          body: error.error.message
+        }
+        this.appc.popToast(msg);
+        return error;
+      }
+    )
+  }
+
+
+  fetchIncome() {
+    
+    this.profitLoss.fetchIncomeDetails(this.getExpenses).subscribe(
+      (data: any) => {
+        this.isRippleLoad = false;
+      },
+      (error: any) => {
+        this.isRippleLoad = false;
+        let msg = {
+          type: "error",
+          body: error.error.message
+        }
+        this.appc.popToast(msg);
+        return error;
+      }
+    )
+  }
 
 }
