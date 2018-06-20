c528caf5848592eb267141f0f02456a534831e1e
ankits1183531 <ankits1183531@gmail.com>
2018-06-20 15:12:05 +0530

    gst report ready

--- src/app/components/report/fee-report/gst-report/gst-report.component.ts
+++ src/app/components/report/fee-report/gst-report/gst-report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ColumnData } from '../../../shared/ng-robAdvanceTable/ng-robAdvanceTable.model';
 import { PaymentHistoryMainService } from '../../../../services/payment-history/payment-history-main.service';
 import * as moment from 'moment';
+import { ExcelService } from '../../../../services/excel.service';
 @Component({
   selector: 'app-gst-report',
   templateUrl: './gst-report.component.html',
@@ -10,9 +11,58 @@ import * as moment from 'moment';
 export class GstReportComponent implements OnInit {
 
 
-  selectMonth: any[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  selectMonth: any[] = [
+    {
+      key: '0',
+      month: "January"
+    },
+    {
+      key: '1',
+      month: "February"
+    },
+    {
+      key: '2',
+      month: "March"
+    },
+    {
+      key: '3',
+      month: "April"
+    },
+    {
+      key: '4',
+      month: "May"
+    },
+    {
+      key: '5',
+      month: "June"
+    },
+    {
+      key: '6',
+      month: "July"
+    },
+    {
+      key: '7',
+      month: "August"
+    },
+    {
+      key: '8',
+      month: "September"
+    },
+    {
+      key: '9',
+      month: "October"
+    },
+    {
+      key: '10',
+      month: "November"
+    },
+    {
+      key: '11',
+      month: "December"
+    }
+  ]
 
-
+  dataStatus: number;
 
   feeSettings1: ColumnData[] = [
     { primaryKey: 'student_disp_id', header: 'ID' },
@@ -40,7 +90,25 @@ export class GstReportComponent implements OnInit {
   }
   getPaymentRecords: any[] = [];
 
-  constructor(private gst: PaymentHistoryMainService) { }
+  downloadService = {
+
+    from_date: moment().format('YYYY-MM-DD'),
+    to_date: moment().format('YYYY-MM-DD'),
+    payment_history_student_category_option: 0,
+    student_name: "",
+    contact_no: "",
+    isExportGSTReport: "Y"
+  }
+
+  downloadFormatted: number;
+
+  searchText = "";
+  searchData = [];
+  searchflag: boolean = false;
+  searchName:string;
+  tempRecords:any[]=[];
+
+  constructor(private gst: PaymentHistoryMainService, private excelService: ExcelService) { }
 
   ngOnInit() {
     this.getGstReport(event);
@@ -48,28 +116,120 @@ export class GstReportComponent implements OnInit {
 
 
   getGstReport(event) {
+
+    this.getPaymentRecords = [];
+
     let date = new Date()
     let y = date.getFullYear()
     let m = date.getMonth();
-    let firstDay = new Date(y,m, 1);
-    let lastDay = new Date(y, m + 1, 0);
-    console.log(moment().endOf(event));
+    let firstDay = new Date(y, parseInt(event), 1);
+    let t = parseInt(event);
+    this.downloadFormatted = t;
+    let lastDay = new Date(y, t + 1, 0);
+    this.dataStatus = 1;
+
     this.sendPayload = {
+
       institute_id: this.gst.institute_id,
-      from_date: moment().startOf(event).format('YYYY-MM-DD'),
-      to_date: moment().endOf(event).format('YYYY-MM-DD'),
+      from_date: moment(firstDay).format('YYYY-MM-DD'),
+      to_date: moment(lastDay).format('YYYY-MM-DD'),
       payment_history_student_category_option: 0,
       student_name: "",
       contact_no: ""
+
     }
+
+
     this.gst.getPaymentData(this.sendPayload).subscribe(
+
       (data: any) => {
+
+        if (data.length == 0) {
+          this.dataStatus = 2;
+        }
+
+        else {
+          this.dataStatus = 0;
+        }
+
         this.getPaymentRecords = data;
+        this.tempRecords = data;
       },
       (error: any) => {
+
         return error;
       }
     )
   }
+
+
+
+  downloadToExcel() {
+
+    let date = new Date();
+
+    this.downloadService = {
+      from_date: moment(new Date(date.getFullYear(), this.downloadFormatted, 1)).format('YYYY-MM-DD'),
+      to_date: moment(new Date(date.getFullYear(), this.downloadFormatted + 1, 0)).format('YYYY-MM-DD'),
+      payment_history_student_category_option: 0,
+      student_name: "",
+      contact_no: "",
+      isExportGSTReport: "Y"
+    }
+
+    this.gst.downloadData(this.downloadService).subscribe(
+
+      (data: any) => {
+        this.exportToExcel(data);
+      }
+    )
+  }
+
+  exportToExcel(event) {
+    let exportedArray: any[] = [];
+    this.getPaymentRecords.map(
+      (data: any) => {
+        let obj = {
+          "Id": data.student_disp_id,
+          "Name": data.student_name,
+          "Reciept No": data.display_invoice_no,
+          "Payment Mode": data.paymentMode,
+          "Fee Type": data.fee_type_name,
+          "Inst No": data.installment_nos,
+          "Paid Date": data.paid_date,
+          "Reference No": data.reference_no,
+          "Amount Paid": data.amount_paid,
+          'CGST Amount' : data.cgst ,
+          'SGST Amount' : data.sgst,
+          'Tax Amount' : data.tax ,
+          "Student_Category": data.student_category,
+          "Counsellor": data.enquiry_counsellor_name
+        }
+        exportedArray.push(obj);
+      }
+      
+    )
+    this.excelService.exportAsExcelFile(
+      exportedArray,
+      'Students'
+    )
+  }
+
+  searchDatabase() {
+    if (this.searchText != "" && this.searchText != null) {
+      // let searchData: any;
+      this.getPaymentRecords = this.getPaymentRecords.filter(item =>
+        Object.keys(item).some(
+          k => item[k] != null && item[k].toString().toLowerCase().includes(this.searchText.toLowerCase()))
+      );
+      // this.searchData = searchData;
+      this.searchflag = true;
+    }
+    else {
+      this.getPaymentRecords = this.tempRecords;
+      this.searchflag = false;
+    }
+  }
+
 
 }
