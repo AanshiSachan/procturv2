1e1ba12b3228273685f3796582104f4c715f9308
https://ronit123321@github.com <ronit.oommen@gmail.com>
2018-06-25 12:36:43 +0530

    custom component issues fixes

--- src/app/components/students/student-add/student-add.component.ts
+++ src/app/components/students/student-add/student-add.component.ts
@@ -476,7 +476,7 @@ export class StudentAddComponent implements OnInit {
   fetchPrefillFormData() {
     this.isRippleLoad = true;
 
-    let inventory = this.studentPrefillService.fetchInventoryList().subscribe(
+    this.studentPrefillService.fetchInventoryList().subscribe(
       data => {
         this.isRippleLoad = false;
         this.inventoryItemsArr = data;
@@ -494,7 +494,7 @@ export class StudentAddComponent implements OnInit {
     );
 
     //this.isRippleLoad = true;
-    let institute = this.prefill.getSchoolDetails().subscribe(
+    this.prefill.getSchoolDetails().subscribe(
       data => {
         this.instituteList = data;
       },
@@ -513,9 +513,10 @@ export class StudentAddComponent implements OnInit {
 
     this.getFeeStructue();
 
-    let standard = this.prefill.getEnqStardards().subscribe(data => {
-      this.standardList = data;
-    },
+    this.prefill.getEnqStardards().subscribe(
+      data => {
+        this.standardList = data;
+      },
       err => {
         let msg = err.error.message;
         this.isRippleLoad = false;
@@ -567,9 +568,9 @@ export class StudentAddComponent implements OnInit {
       }
     )
 
-    if (inventory != null && institute != null && standard != null) {
-      let customComp = this.studentPrefillService.fetchCustomComponent().subscribe(
-        data => {
+    this.studentPrefillService.fetchCustomComponent().subscribe(
+      data => {
+        if(data != null){
           data.forEach(el => {
 
             let obj = {
@@ -642,24 +643,20 @@ export class StudentAddComponent implements OnInit {
             }
             this.customComponents.push(obj);
           });
-          this.isRippleLoad = false;
-        },
-        err => {
-          let msg = err.error.message;
-          this.isRippleLoad = false;
-          let obj = {
-            type: 'error',
-            title: msg,
-            body: ""
-          }
-          this.appC.popToast(obj);
         }
-      );
-
-      //console.log(this.customComponents);
-      return customComp;
-    }
-
+        this.isRippleLoad = false;
+      },
+      err => {
+        let msg = err.error.message;
+        this.isRippleLoad = false;
+        let obj = {
+          type: 'error',
+          title: msg,
+          body: ""
+        }
+        this.appC.popToast(obj);
+      }
+    );
   }
 
   /* ============================================================================================================================ */
@@ -2679,7 +2676,7 @@ export class StudentAddComponent implements OnInit {
     }
     return distinct;
   }
-  
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   updateStudentFeeDetails() {
@@ -2867,8 +2864,8 @@ export class StudentAddComponent implements OnInit {
     this.instalmentTableData[i].due_date = event;
     this.updateTableInstallment();
   }
-  
-  
+
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   updateTableInstallment() {
@@ -2969,7 +2966,7 @@ export class StudentAddComponent implements OnInit {
     }
   }
 
-  
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   studentAdder() {
@@ -3174,8 +3171,8 @@ export class StudentAddComponent implements OnInit {
       return 0;
     }
   }
-  
-  
+
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   allocateInventory(id) {
@@ -3460,7 +3457,7 @@ export class StudentAddComponent implements OnInit {
     }
   }
 
-  
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   getFeeStructure(fee: any[]): any[] {
@@ -3519,7 +3516,7 @@ export class StudentAddComponent implements OnInit {
     }
   }
 
-  
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   validPdc(obj): boolean {
@@ -3662,7 +3659,7 @@ export class StudentAddComponent implements OnInit {
     this.selectedCheque = i;
   }
 
-  
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   editPDC(data) {
@@ -3863,8 +3860,8 @@ export class StudentAddComponent implements OnInit {
       return true;
     }
   }
- 
- 
+
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
   /* convert base64 string to byte array */
@@ -4970,7 +4967,7 @@ export class StudentAddComponent implements OnInit {
 
 
   /* ============================================================================================================================ */
-  /* ============================================================================================================================ */  
+  /* ============================================================================================================================ */
   getCourseDropdown(id) {
     this.fetchService.getStudentCourseDetails(id).subscribe(
       res => {
@@ -4982,7 +4979,7 @@ export class StudentAddComponent implements OnInit {
       }
     )
   }
-  
+
   /* ============================================================================================================================ */
   /* ============================================================================================================================ */
 
