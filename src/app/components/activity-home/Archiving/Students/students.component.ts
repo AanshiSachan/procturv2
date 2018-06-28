987abb9283abe9d61810b410ca4fdeebfe3b6012
ashi0711 <ankits1183531@gmail.com>
2018-06-28 18:32:33 +0530

    students data report

--- src/app/components/activity-home/Archiving/Students/students.component.ts
+++ src/app/components/activity-home/Archiving/Students/students.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesServiceService } from '../../../../services/archiving-service/courses-service.service';
+import { AppComponent } from '../../../../app.component';
 
 @Component({
   selector: 'app-students',
@@ -7,9 +9,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsComponent implements OnInit {
 
-  constructor() { }
+  getStudents:any[]=[];
+  PageIndex: number = 1;
+  PageIndexPopup: number = 1;
+  pagedisplaysize: number = 10;
+  pagedisplaysizePopup: number = 10;
+  totalRow: number = 0;
+  newPaginated: any[] = [];
+  searchText: string = ""
+  searchData: any[] = [];
+  searchflag: boolean = false;
+  dummyArr: any[] = [0, 1, 2, 0, 1, 2];
+  columnMaps: any[] = [0, 1, 2, 3, 4, 5];
+  dataStatus: boolean;
+
+
+  constructor(private students: CoursesServiceService,
+    private appc: AppComponent) { }
 
   ngOnInit() {
+    this.studentsData();
   }
+
+  studentsData() {
+    this.dataStatus = true;
+     this.students.studentsArchiveData().subscribe(
+      (data: any) => {
+        this.dataStatus = false;
+        this.getStudents = data;
+        this.totalRow = data.length;
+        this.PageIndex = 1;
+        this.fetchTableDataByPage(this.PageIndex);
+      },
+      (error: any) => {
+        this.dataStatus = false;
+        let msg = {
+          type: "error",
+          body: error.error.message
+        }
+        this.appc.popToast(msg);
+      }
+    )
+  }
+
+  searchDatabase() {
+    if (this.searchText != "" && this.searchText != null) {
+      this.PageIndex = 1;
+      let searchRes: any;
+      searchRes = this.getStudents.filter(item =>
+        Object.keys(item).some(
+          k => item[k] != null && item[k].toString().toLowerCase().includes(this.searchText.toLowerCase()))
+      );
+      this.searchData = searchRes;
+      this.totalRow = searchRes.length;
+
+      this.searchflag = true;
+      this.fetchTableDataByPage(this.PageIndex);
+    }
+    else {
+      this.searchflag = false;
+      this.fetchTableDataByPage(this.PageIndex);
+      this.totalRow = this.getStudents.length;
+    }
+  }
+
+
+  fetchTableDataByPage(index) {
+    this.PageIndex = index;
+    let startindex = this.pagedisplaysize * (index - 1);
+    this.newPaginated = this.getDataFromDataSource(startindex);
+
+  }
+
+  fetchNext() {
+    this.PageIndex++;
+    this.fetchTableDataByPage(this.PageIndex);
+  }
+
+
+  fetchPrevious() {
+    if (this.PageIndex != 1) {
+      this.PageIndex--;
+      this.fetchTableDataByPage(this.PageIndex);
+    }
+  }
+
+  getDataFromDataSource(startindex) {
+    if (this.searchflag) {
+      let t = this.searchData.slice(startindex, startindex + this.pagedisplaysize);
+      return t;
+    } else {
+      let t = this.getStudents.slice(startindex, startindex + this.pagedisplaysize);
+      return t;
+    }
+  }
+
+
 
 }
