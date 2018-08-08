3cbb71720e28f5844a702a7c4c8d6fe81d70699f
Ayushi <ankits1183531@gmail.com>
2018-08-08 17:30:28 +0530

    time table issue

--- src/app/components/report/time-table/table/table.component.ts
+++ src/app/components/report/time-table/table/table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef, Input, OnChanges } from '@ang
 import { timeTableService } from '../../../../services/TimeTable/timeTable.service';
 import { AppComponent } from '../../../../app.component';
 import * as moment from 'moment';
+// var jsPDF = require('jspdf');
+// require('jspdf-autotable');
 
 @Component({
   selector: 'time-table',
@@ -26,12 +28,53 @@ export class tableComponent {
     }
   }
 
-  splitBatchData(batch_name) {
-    if (batch_name.length >= 20) {
-      return (batch_name.slice(0, 16) + "..");
-    }
-    else {
-      return batch_name;
-    }
-  }
+  // generate() {
+  //   let doc = new jsPDF('l', 'pt');
+  //   let columns = ["ID", "Name", "Country"];
+  //   let rows = [
+  //     [1, "Shaw", "Tanzania"],
+  //     [2, "Nelson", "Kazakhstan"],
+  //     [3, "Garcia", "Madagascar"]]
+  //   doc.autoTable(columns, rows);
+  //   doc.save('table.pdf');
+  // }
+
+  // generate() {
+
+  //   var doc = new jsPDF('l', 'mm');
+
+  //   let columns = [['date' , 'time' ,  'months']]
+  //   let rows = ['tody' , 'tomorrow' , 'dayafter']
+  //   doc.autoTable(columns, rows, {margin: {top: 80} , styles:{cellWidth:35}});
+
+  //   // var header = function(data) {
+  //   //   doc.setFontSize(18);
+  //   //   doc.setTextColor(40);
+  //   //   doc.setFontStyle('normal');
+  //   //   //doc.addImage(headerImgData, 'JPEG', data.settings.margin.left, 20, 50, 50);
+  //   //   doc.text("Testing Report", data.settings.margin.left, 50);
+  //   // };
+
+  //   // var options = {
+  //   //   styles:{cellWidth:60},
+  //   //   addPageContent: header,
+  //   //   margin: {
+  //   //     top: 80
+  //   //   },
+  //   //   startY: doc.doc.previousAutoTable.finalY() + 20
+  //   // };
+
+  //   // doc.autoTable(columns, rows, options);
+
+  //   doc.save("table.pdf");
+  // }
+
+  // splitBatchData(batch_name) {
+  //   if (batch_name.length >= 20) {
+  //     return (batch_name.slice(0, 16) + "..");
+  //   }
+  //   else {
+  //     return batch_name;
+  //   }
+  // }
 }
