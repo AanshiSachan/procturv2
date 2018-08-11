b8ed0a1ee5e4da3dc43d0066a84411da64aa9622
proctur.dev4 <proctur.dev4@gmail.com>
2018-08-11 18:45:32 +0530

    installment revert issue fixes

--- src/app/components/activity-home/home/home.component.ts
+++ src/app/components/activity-home/home/home.component.ts
@@ -1,19 +1,6 @@
-import {
-  Component, OnInit, ViewChild, Input, Output, EventEmitter, HostListener,
-  AfterViewInit, OnDestroy, ElementRef, Renderer2, ChangeDetectionStrategy, ChangeDetectorRef,
-  SimpleChanges, OnChanges
-} from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl, ValidatorFn } from '@angular/forms';
-import { AppComponent } from '../../../app.component';
-import * as moment from 'moment';
-import { MenuItem } from 'primeng/primeng';
-import { Pipe, PipeTransform } from '@angular/core';
 import { LoginService } from '../../../services/login-services/login.service';
-import { document } from '../../../../assets/imported_modules/ngx-bootstrap/utils/facade/browser';
-import { ColumnSetting } from '../../shared/custom-table/layout.model';
-import { Observable } from 'rxjs/Rx';
-import { Subscription } from 'rxjs';
 import 'rxjs/Rx';
 import { AuthenticatorService } from '../../../services/authenticator.service';
 
@@ -26,11 +13,11 @@ export class HomeComponent implements OnInit {
 
   isProfessional: boolean = false;
   isAdmin: boolean = false;
-
   isFeeActivity: boolean = false;
   isMonitorDashboard: boolean = false;
+  showExamDesk: boolean = false;
 
-  constructor(private router: Router, private fb: FormBuilder, private appC: AppComponent, private login: LoginService, private rd: Renderer2, private cd: ChangeDetectorRef, private auth: AuthenticatorService) {
+  constructor(private router: Router, private login: LoginService, private auth: AuthenticatorService) {
     if (sessionStorage.getItem('userid') == null) {
       this.router.navigateByUrl('/authPage');
     }
@@ -50,10 +37,13 @@ export class HomeComponent implements OnInit {
     this.login.changeInstituteStatus(sessionStorage.getItem('institute_name'));
     this.login.changeNameStatus(sessionStorage.getItem('name'));
     this.checkUserAccess();
+    if (this.isAdmin) {
+      let type = Number(sessionStorage.getItem('institute_setup_type'));
+      this.showExamDesk = this.checkInstSetupType(type, 4);
+    }
   }
 
   checkUserAccess() {
-
     const permissionArray = sessionStorage.getItem('permissions');
     const userType = sessionStorage.getItem('userType');
     if (userType == '3') {
@@ -75,5 +65,69 @@ export class HomeComponent implements OnInit {
     }
 
   }
+
+  checkInstSetupType(value, role): boolean {
+    if (value != 0) {
+      var start = 2;
+      var count = 1;
+      while (start != value) {
+        count++;
+        start = start + 2;
+      }
+      var arr = [0, 0, 0, 0, 0, 0, 0, 0];
+      var s = count.toString(2);
+      var k = 0;
+      for (var i = s.length - 1; i >= 0; i--) {
+        arr[k] = parseInt(s.charAt(i));
+        k++;
+      }
+
+      switch (role) {
+        case 2:
+          if (arr[0] == 1)
+            return true;
+          break;
+
+        case 4:
+          if (arr[1] == 1)
+            return true;
+          break;
+
+        case 8:
+          if (arr[2] == 1)
+            return true;
+          break;
+
+        case 16:
+          if (arr[3] == 1)
+            return true;
+          break;
+        case 32:
+          if (arr[4] == 1)
+            return true;
+          break;
+        case 64:
+          if (arr[5] == 1)
+            return true;
+          break;
+
+        case 128:
+          if (arr[6] == 1)
+            return true;
+          break;
+        case 256:
+          if (arr[7] == 1)
+            return true;
+          break;
+        default: return false;
+      }
+      return false;
+
+    }
+    else {
+      return false;
+    }
+  }
+
 
 }
