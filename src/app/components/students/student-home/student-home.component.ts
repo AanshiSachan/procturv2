368bc99b9d4a0a83efee2e38f98efde7e578b5fb
laxmi-Wapte <laxmi@proctur.com>
2018-10-31 14:36:10 +0530

    label change

--- src/app/components/students/student-home/student-home.component.ts
+++ src/app/components/students/student-home/student-home.component.ts
@@ -129,7 +129,7 @@ export class StudentHomeComponent implements OnInit {
             { primaryKey: 'student_disp_id', header: 'Student Id' },
             { primaryKey: 'student_name', header: 'Name' },
             { primaryKey: 'student_phone', header: 'Contact No' },
-            { primaryKey: 'student_class', header: 'Standard' },
+            { primaryKey: 'student_class', header: 'Master Course' },
             { primaryKey: 'batchesAssigned', header: 'Batch Assigned' }
           ];
         }
