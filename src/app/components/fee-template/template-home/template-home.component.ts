0b27a90ea9c6e29abc1048120e70ae7038cfc12c
swapnil-proctur <swapnil.proctur@gmail.com>
2019-06-26 19:19:03 +0530

    dashboard issue fixed

--- src/app/components/fee-template/template-home/template-home.component.ts
+++ src/app/components/fee-template/template-home/template-home.component.ts
@@ -154,6 +154,7 @@ export class TemplateHomeComponent implements OnInit {
   }
 
   fillFeeType(data) {
+    this.otherFeetype = [];
     let keys = Object.keys(data);
     for (let i = 0; i < keys.length; i++) {
       let test: any = {};
