039a8d028643430b9391505ccfc3a9d446a2862c
swapnil-proctur <swapnil.proctur@gmail.com>
2019-07-16 19:38:01 +0530

    UI Changes and Topic external module removed

--- src/app/components/master/master-routing.module.ts
+++ src/app/components/master/master-routing.module.ts
@@ -7,7 +7,6 @@ import { SlotComponent } from '../../components/slot/slot.component';
 import { CityAreaMapComponent } from '../../components/city-area-map/city-area-map.component';
 import { ClassRoomComponent } from '../../components/class-room/class-room.component';
 import { EventManagmentComponent } from '../../components/event-managment/event-managment.component';
-import { TopicsComponent } from '../../components/topics/topics.component';
 import { ManageExamGradesComponent } from './manage-exam-grades/manage-exam-grades.component';
 import { ClosingReasonComponent } from './closing-reason/closing-reason.component';
 import { DiscountReasonComponent } from '../discount-reason/discount-reason.component';
@@ -34,12 +33,6 @@ import { DiscountReasonComponent } from '../discount-reason/discount-reason.comp
                         {
                             path: 'classroom',
                             component: ClassRoomComponent,
-                            canLoad: [AuthGuard]
-                        },
-                        {
-
-                            path: 'Topics',
-                            component: TopicsComponent,
                             canLoad: [AuthGuard]
                         },
                         {
