96befe80ee39b8b59749880aede766a4fa8e7ee5
laxmi-Wapte <laxmi@proctur.com>
2019-08-10 19:19:28 +0530

    uplaod marks issue fixed

--- src/app/components/homepage-dashboard/exam-mark-update/exam-mark-update.component.ts
+++ src/app/components/homepage-dashboard/exam-mark-update/exam-mark-update.component.ts
@@ -482,7 +482,9 @@ export class ExamMarkUpdateComponent implements OnInit {
 
   markAllCheckBoxClick(event) {
     this.studentAttList.forEach(element => {
-      element.assigned = event.target.checked;
+      if(element.attendance=='P'){
+        element.assigned = event.target.checked;
+      }      
     });
   }
 
