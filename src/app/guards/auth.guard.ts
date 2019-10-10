ac9f26c46180972c9c359439b007513f41dbb237
laxmi-wapte <laxmi@proctur.com>
2019-10-10 15:10:02 +0530

    enquiery issue fixed

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -228,7 +228,7 @@ export class AuthGuard implements CanActivate, CanLoad {
                 JSON.parse(sessionStorage.getItem('permissions')).includes('206') ||
                 JSON.parse(sessionStorage.getItem('permissions')).includes('207') ||
                 JSON.parse(sessionStorage.getItem('permissions')).includes('208') ||
-                JSON.parse(sessionStorage.getItem('permissions')).includes('723')) {
+                JSON.parse(sessionStorage.getItem('permissions')).includes('722')) {
                 return true;
             }
             else {
