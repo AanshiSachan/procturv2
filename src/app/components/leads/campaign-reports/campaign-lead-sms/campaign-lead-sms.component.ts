cc18b5d3d5ed9e9de714652c3cac21907a9a2906
swapnil-proctur <swapnil.proctur@gmail.com>
2019-12-30 12:35:42 +0530

    API inte. and UI dev.

--- src/app/components/leads/campaign-reports/campaign-lead-sms/campaign-lead-sms.component.ts
+++ src/app/components/leads/campaign-reports/campaign-lead-sms/campaign-lead-sms.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { MessageShowService } from '../../../../services/message-show.service';
 import { getSMSService } from '../../../../services/report-services/get-sms.service';
+import { CampaignService } from '../../services/campaign.service';
+import { AuthenticatorService } from '../../../../services/authenticator.service';
 
 @Component({
   selector: 'app-campaign-lead-sms',
@@ -18,26 +20,56 @@ export class CampaignLeadSmsComponent implements OnInit {
 
   dateFilter = {
     from_date: moment(new Date()).format('YYYY-MM-DD'),
-    to_date: ""
+    to_date: "",
+    institution_id: "",
+    start_index: 0,
+    batch_size: 50
   };
 
+  smsTotalCount: number = 0;
+  smsSuccessCount: number = 0;
+  smsFailedCount: number = 0;
+
+  leadSmsSearchInput: any = "";
+  leadSmsList: any;
+  tempLeadSmslist: any;
+
+  // FOR PAGINATION
+  pageIndex: number = 1;
+  displayBatchSize: number = 50;
+  totalCount: number = 0;
+  startindex: number = 0;
+
   constructor(
+    private campaignService: CampaignService,
     private _msgService: MessageShowService,
     private getSms: getSMSService,
+    private auth: AuthenticatorService,
   ) { }
 
   ngOnInit() {
+    this.dateFilter.institution_id = sessionStorage.getItem('institute_id');
     this.getSmsReport(this.dateFilter);
-
   }
 
   getSmsReport(obj) {
     this.jsonFlag.isRippleLoad = true;
     if (obj.start_index == 0) {
-      return this.getSms.fetchSmsReport(obj).subscribe(
+      return this.campaignService.fetchSmsReport(obj).subscribe(
         (res: any) => {
+          let result: any;
+          result = res;
           this.jsonFlag.isRippleLoad = false;
-
+          this.pageIndex = 1;
+          this.totalCount = 0;
+          this.leadSmsList = res;
+          this.tempLeadSmslist = res;
+          if(result.length > 0){
+            this.totalCount = this.leadSmsList[0].total_count;
+            this.smsTotalCount = this.leadSmsList[0].total_count;
+            this.smsSuccessCount = this.leadSmsList[0].successful_count;
+            this.smsFailedCount = this.leadSmsList[0].failure_count;
+          }
         },
         err => {
           this.jsonFlag.isRippleLoad = false;
@@ -45,7 +77,7 @@ export class CampaignLeadSmsComponent implements OnInit {
       )
     }
     else {
-      return this.getSms.fetchSmsReport(obj).subscribe(
+      return this.campaignService.fetchSmsReport(obj).subscribe(
         (res: any) => {
           this.jsonFlag.isRippleLoad = false;
         }
@@ -54,6 +86,7 @@ export class CampaignLeadSmsComponent implements OnInit {
   }
 
   fetchSmsByDate() {
+    this.dateFilter.start_index = 0;
     this.getSmsReport(this.dateFilter);
   }
 
@@ -71,5 +104,50 @@ export class CampaignLeadSmsComponent implements OnInit {
       this._msgService.showErrorMessage(this._msgService.toastTypes.info, '', "Future date is not allowed");
     }
   }
+
+  searchDatabase(){   // quick search
+    this.leadSmsList = this.tempLeadSmslist;
+    if (this.leadSmsSearchInput == undefined || this.leadSmsSearchInput == null) {
+      this.leadSmsSearchInput = "";
+    }
+    else {
+      let searchData = this.tempLeadSmslist.filter(item =>
+        Object.keys(item).some(
+          k => item[k] != null && item[k].toString().toLowerCase().includes(this.leadSmsSearchInput.toLowerCase()))
+      );
+      this.leadSmsList = searchData;
+    }
+  }
+
+
+  /*** pagination functions */
+  /* Fetch next set of data from server and update table */
+  fetchNext() {
+    this.pageIndex++;
+    this.fectchTableDataByPage(this.pageIndex);
+  }
+
+  /* Fetch previous set of data from server and update table */
+  fetchPrevious() {
+    this.pageIndex--;
+    this.fectchTableDataByPage(this.pageIndex);
+  }
+
+  /* Fetch table data by page index */
+  fectchTableDataByPage(index) {
+    this.pageIndex = index;
+    let startindex = this.displayBatchSize * (index - 1);
+    this.dateFilter.start_index = startindex;
+    this.getSmsReport(this.dateFilter);
+  }
+
+  /* Fetches Data as per the user selected batch size */
+  updateTableBatchSize(num) {
+    this.pageIndex = 1;
+    this.displayBatchSize = parseInt(num);
+    this.dateFilter.start_index = this.startindex;
+    this.getSmsReport(this.dateFilter);
+  }
+
 
 }
