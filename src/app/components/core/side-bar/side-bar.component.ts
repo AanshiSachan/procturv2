879b42caca8b2004f134c04996d95f52afa54227
swapnil-proctur <swapnil.bhavsar@proctur.com>
2020-03-13 21:05:53 +0530

    allow to specific institute

--- src/app/components/core/side-bar/side-bar.component.ts
+++ src/app/components/core/side-bar/side-bar.component.ts
@@ -87,7 +87,8 @@ export class SideBarComponent implements OnInit, AfterViewInit {
     isShoweStore: false,
     isShoweOnlineExam: false,
     isAdmin: false,
-    isShowPowerBy: false
+    isShowPowerBy: false,
+    isShowExpense: false
   }
 
 
@@ -451,7 +452,7 @@ export class SideBarComponent implements OnInit, AfterViewInit {
     this.isLiveClassesAllow(type);
     this.isElearnAllow();
     this.isLibraryFeatureAllow(permission); // check librabry feature
-
+    this.isExpenseFeatureAllow();
   }
 
   // check only default values
@@ -484,6 +485,19 @@ export class SideBarComponent implements OnInit, AfterViewInit {
       (username == "admin" && this.instituteId == 100135) ||
       (permission && permission.indexOf('721') != -1)) {
       this.jsonFlags.isShowLibrabry = true;
+    }
+  }
+
+  isExpenseFeatureAllow(){
+    this.jsonFlags.isShowExpense = false;
+    if (this.instituteId == 101238 ||
+        this.instituteId == 101242 ||
+        this.instituteId == 101243 ||
+        this.instituteId == 101244 ||
+        this.instituteId == 100058 ||
+        this.instituteId == 100127 ||
+        this.instituteId == 100126) {
+      this.jsonFlags.isShowExpense = true;
     }
   }
 
