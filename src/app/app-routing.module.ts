b29f4202d9075ab26feb07adf1ed373c63eee7ef
Abhishek kumar <proctur.dev1@gmail.com>
2020-06-04 15:37:44 +0530

    login page performance issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,40 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule } from '@angular/router';
+import { CustomPreloadingStrategy } from './custom-preloading-strategy.service';
 
 @NgModule({
     imports: [
         RouterModule.forRoot(
             [
-                { path: '', redirectTo: '/authPage', pathMatch: 'full' },
+                { 
+                    path: '', redirectTo: '/authPage', pathMatch: 'full',
+                    data:{preload: true}
+                 },
                 {
                     path: 'authPage',
-                    loadChildren: 'app/components/auth-page/auth-page.module#AuthPageModule'
+                    loadChildren: 'app/components/auth-page/auth-page.module#AuthPageModule',
+                    data:{preload: true}
                 },
                 {
                     path: 'guest',
-                    loadChildren: 'app/components/guest-user/guest-user.module#GuestUserModule'
+                    loadChildren: 'app/components/guest-user/guest-user.module#GuestUserModule',
+
                 },
                 {
                     path: 'view',
-                    loadChildren: 'app/components/component.module#ComponentModule'
+                    loadChildren: 'app/components/component.module#ComponentModule',
+
                 },
                 { path: '**',  redirectTo: '/authPage', pathMatch: 'full' }
             ],
             {
                 useHash: true,
-                preloadingStrategy: PreloadAllModules
+                preloadingStrategy: CustomPreloadingStrategy
             }
         )
     ],
-    exports: [
-        RouterModule
-    ],
-    providers: [
-        /* CanDeactivateGuard */
-    ]
+    exports: [RouterModule],
+    providers: [CustomPreloadingStrategy]
 })
 export class AppRoutingModule {
 }
