4439cf8507b59df48e9842d9b88fc99e3784f117
naliniProctur <nalini@proctur.com>
2020-07-20 18:43:42 +0530

    Guest filter added in guest list

--- src/app/components/live-classes-module/attendance-report/attendance-report.component.ts
+++ src/app/components/live-classes-module/attendance-report/attendance-report.component.ts
@@ -69,9 +69,11 @@ export class AttendanceReportComponent implements OnInit {
         var res: any = data.result;
         this.invited_attendance_list = res.invited_attendance_list;
         this.guest_attendance_list = res.guest_attendance_list;
+        this.guest_attendance_list.forEach(elem=>{
+          elem.isInvited = true;
+        })
         let arr = this.guest_attendance_list.concat(this.invited_attendance_list);
         this.attendance_list = arr.sort((a, b) => a.name.toLocaleUpperCase() < b.name.toLocaleUpperCase() ? -1 : 1);
-
         this.temp_attendance_list = this.attendance_list;
         this.session_data = res.statistics;
       },
