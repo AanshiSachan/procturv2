cd3e4b7e2c49bc71ef4efaad1c8042a7452140ed
naliniProctur <nalini@proctur.com>
2020-11-02 19:00:47 +0530

    change vimeo category id to 305

--- src/app/components/course-module/file-manager/upload-popup/upload-popup.component.ts
+++ src/app/components/course-module/file-manager/upload-popup/upload-popup.component.ts
@@ -59,7 +59,7 @@ export class UploadPopupComponent implements OnInit, OnChanges {
   category_id: number | string = "-1";
   youtubeUrl: any = '';
   is_readonly: any = '';
-  vimeo_category_id = '272';
+  vimeo_category_id = '305';
   fileUploadXHR: any = '';
 
   category_image = {
@@ -150,7 +150,7 @@ export class UploadPopupComponent implements OnInit, OnChanges {
       rtf: "7",
       zip: '8'
     },
-    272: {
+    305: {
       avi: '1',
       flv: '2',
       wmv: '3',
