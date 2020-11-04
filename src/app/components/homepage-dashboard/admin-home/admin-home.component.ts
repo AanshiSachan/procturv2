b44747a0f915feaa6bb0c2f28fa4162498c468f7
naliniProctur <nalini@proctur.com>
2020-11-04 17:51:13 +0530

    issue fixed

--- src/app/components/homepage-dashboard/admin-home/admin-home.component.ts
+++ src/app/components/homepage-dashboard/admin-home/admin-home.component.ts
@@ -2761,18 +2761,13 @@ export class AdminHomeComponent implements OnInit {
   //  Role Based Access
   checkIfUserHadAccess(id) {
     this.permissionArray = sessionStorage.getItem('permissions');
-    if (this.permissionArray == "" || this.permissionArray == null) {
+    if (this.permissionArray == "" || this.permissionArray == null || !this.permissionArray) {
       return false;
     } else {
-      let data = JSON.parse(this.permissionArray);
-      if (id != "" && data != null && data != "") {
-        if (data.indexOf(id) == "-1") {
-          return true;
-        } else {
-          return false;
-        }
+      if(id) {
+        return false;
       } else {
-        return '';
+       return true;
       }
     }
   }
