b95951b3e54975c1efde173516109d426d00a7bf
naliniProctur <nalini@proctur.com>
2020-12-21 18:36:39 +0530

    Ecourse changes

--- src/app/components/course-module/ecourse-mapping/ecourse-mapping.component.ts
+++ src/app/components/course-module/ecourse-mapping/ecourse-mapping.component.ts
@@ -32,7 +32,11 @@ export class EcourseMappingComponent implements OnInit {
       master_course_ids: "",
       master_course_names: "",
       is_online: 'Y',
-      status: 'Y'
+      status: 'Y',
+      is_popular: false,
+      long_description: '',
+      short_description: '',
+      thumbnail_url: null
     }
 
   displayKeys: any[] = [
@@ -45,6 +49,9 @@ export class EcourseMappingComponent implements OnInit {
   tempBatchList: any[] = [];
   assignCourses: any[] = [];
   statusOption: any = false;
+  editView: any = false;
+  thumbnailData: any[] = [];
+  thumbnail_image:any;
   //table setting
   tableSetting: any = {//inventory.item
     tableDetails: { title: 'Ecourse mapping', key: 'reports.fee.ecoursemapping', showTitle: false },
@@ -60,6 +67,19 @@ export class EcourseMappingComponent implements OnInit {
       { title: "Edit", class: 'fa fa-check updateCss', optionType: 'edit' }],
     },
     displayMessage: "Enter Detail to Search"
+  };
+  editorConf = {
+    height: 150,
+    menubar: false,
+    branding: false,
+    plugins: [
+      'preview anchor',
+      'visualblocks code ',
+      'insertdatetime  table paste code  wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | bold italic backcolor | \
+              alignleft aligncenter alignright alignjustify | \
+              bullist numlist outdent indent'
   };
 
 
@@ -108,13 +128,16 @@ export class EcourseMappingComponent implements OnInit {
         }
       }
     )
+    this.getDefaultThumbnail();
 
   }
 
   getListOfCourses() {
     this.tempBatchList = [];
+    this.auth.showLoader();
     let url = "/api/v1/courseMaster/fetch/" + this.institute_id + "/all";
     this._http.getData(url).subscribe((res: any) => {
+      this.auth.hideLoader();
       console.log(res);
       this.tempBatchList = res;
       this.courseMapingObject(res);
@@ -166,23 +189,36 @@ export class EcourseMappingComponent implements OnInit {
   addEcourseMapping() {
     let url = '/api/v1/institute/courseMapping/' + this.institute_id;
     let data = [];
-    if (this.ecourseObject.course_type == undefined || this.ecourseObject.course_type.trim() == '') {
+    if (this.updateEcourseObject.course_type == undefined || this.updateEcourseObject.course_type.trim() == '') {
       this._msgService.showErrorMessage('error', '', 'please enter E-course name !');
       return;
     }
 
+    if (this.ecourseObject.status == 'Online') {
+      this.ecourseObject.status = 'Y';
+    } else {
+      this.ecourseObject.status = 'N';
+    }
+
     let obj = {
-      course_type: this.ecourseObject.course_type,
+      course_type: this.updateEcourseObject.course_type,
       course_type_id: 0,
       is_test_series: "y",
       master_course_ids: this.ecourseObject.master_course_ids,
-      is_online : this.ecourseObject.is_online
+      is_online : this.ecourseObject.status,
+      short_description: this.updateEcourseObject.short_description,
+      long_description: this.updateEcourseObject.long_description,
+      is_popular: this.updateEcourseObject.is_popular,
+      thumbnail_image: this.thumbnail_image,
+      thumbnail_url:this.updateEcourseObject.thumbnail_url
     }
+    obj.thumbnail_url = (obj.thumbnail_url == '') ? null : obj.thumbnail_url;
     data.push(obj);
     console.log(this.ecourseObject, obj);
     this._http.postData(url, data).subscribe((res) => {
       let msg = this.jsonflag.isProfessional ? 'Batch Added Successfully ' : 'Course Added Successfully';
       this._msgService.showErrorMessage('success', '', 'Course Added Successfully');
+      this.updateEcourseObject = {};
       this.clearObjects();
       this.ecourseData = [];
       this.getEcourseMappingData();
@@ -203,18 +239,34 @@ export class EcourseMappingComponent implements OnInit {
         master_course_names: "",
         master_course_ids: "",
         is_online: '',
-        status: 'Y'
+        status: 'Y',
+        is_popular: false,
+        long_description: '',
+        short_description: '',
+        thumbnail_url: null
       }
+      this.updateEcourseObject.course_type = '';
+      this.updateEcourseObject.is_popular = false;
+      this.updateEcourseObject.long_description = '';
+      this.updateEcourseObject.short_description = '';
+      this.thumbnail_image = '';
+      this.updateEcourseObject.thumbnail_url = null;
+      this.batchList.forEach((obj) => obj.isSelected = false);
   }
 
 
   optionSelected($event) {
+    console.log($event);
     this.jsonflag.isUpadted = true;
     switch ($event.type) {
       case 'assign': {
         this.updateEcourseObject = Object.assign({}, $event.data);// copy the object instead get reference to that object
         this.updateEcourseObject.master_course_names = $event.data.assignCoursesId;
         this.ecourseObject.status = $event.data.is_online;
+        this.updateEcourseObject.short_description = $event.data.short_description;
+        this.updateEcourseObject.long_description = $event.data.long_description;
+        this.updateEcourseObject.is_popular = $event.data.is_popular;
+        this.updateEcourseObject.thumbnail_url = $event.data.thumbnail_url;
         this.batchList.forEach((obj) => obj.isSelected = false);
         if (this.updateEcourseObject.master_course_names) {
           let names = this.updateEcourseObject.master_course_names.split(",");
@@ -258,9 +310,25 @@ export class EcourseMappingComponent implements OnInit {
       }
 
       case 'edit': {
+        this.editView = true;
+        // this.getDefaultThumbnail();
         this.updateEcourseObject = Object.assign({}, $event.data);// copy the object instead get reference to that object
         this.updateEcourseObject.master_course_names = $event.data.assignCoursesId;
         this.ecourseObject.status = $event.data.is_online;
+        this.updateEcourseObject.short_description = $event.data.short_description;
+        this.updateEcourseObject.long_description = $event.data.long_description;
+        this.updateEcourseObject.is_popular = $event.data.is_popular;
+        if(this.updateEcourseObject.thumbnail_url != '' && this.updateEcourseObject.thumbnail_url != null) {
+          this.updateEcourseObject.thumbnail_url = $event.data.thumbnail_url + '?' + Math.random().toFixed(2);
+        }
+        if(this.thumbnailData && this.thumbnailData.length) {
+          this.thumbnailData.forEach(obj => {
+            obj.isSelected = false;
+            if(this.updateEcourseObject.thumbnail_url != '' && this.updateEcourseObject.thumbnail_url != null && this.updateEcourseObject.thumbnail_url == obj.data_value) {
+              obj.isSelected = true;
+            }
+          })
+        }
         this.batchList.forEach((obj) => obj.isSelected = false);
         if (this.updateEcourseObject.master_course_names) {
           let names = this.updateEcourseObject.master_course_names.split(",");
@@ -313,11 +381,19 @@ export class EcourseMappingComponent implements OnInit {
       course_type_id: this.updateEcourseObject.course_type_id,
       is_test_series: "Y",
       master_course_ids: this.updateEcourseObject.master_course_ids,
-      is_online: this.ecourseObject.status
+      is_online: this.ecourseObject.status,
+      short_description: this.updateEcourseObject.short_description,
+      long_description: this.updateEcourseObject.long_description,
+      is_popular: this.updateEcourseObject.is_popular,
+      thumbnail_url: this.updateEcourseObject.thumbnail_url,
+      thumbnail_image: this.thumbnail_image
     }
+    obj.thumbnail_url = (obj.thumbnail_url == '') ? null : obj.thumbnail_url;
     // data.push(obj);
     console.log(this.updateEcourseObject, data);
+    this.auth.showLoader();
     this._http.putData(url, obj).subscribe((res) => {
+      this.auth.hideLoader();
       let msg = this.jsonflag.isProfessional ? 'Batch updated Successfully ' : 'Course updated Successfully';
       this._msgService.showErrorMessage('success', '', msg);
       this.ecourseData = [];
@@ -328,6 +404,7 @@ export class EcourseMappingComponent implements OnInit {
       this.updateEcourseObject = {};
       this.clearObjects();
     }, (err) => {
+      this.auth.hideLoader();
       this._msgService.showErrorMessage('error', '', err.error.message);
     });
   }
@@ -357,7 +434,7 @@ export class EcourseMappingComponent implements OnInit {
   addCourseOrStandard() {
     if(this.jsonflag.allowMapping == '1'){
     let selectedData = this.batchList.filter((data) => data.isSelected == true);
-    if (!selectedData.length) {
+    if (!selectedData.length && this.jsonflag.isUpadted) {
       let msg = this.jsonflag.isProfessional ? 'please select batch' : 'please select course';
       this._msgService.showErrorMessage('error', '', msg);
     }
@@ -376,16 +453,25 @@ export class EcourseMappingComponent implements OnInit {
         }
       });
       this.assignCourses = this.ecourseObject.master_course_names.split(',');
+      console.log(this.updateEcourseObject.master_course_ids);
       this.jsonflag.isAssignBatch = false;
-      if (this.jsonflag.isUpadted) {
+      if((this.updateEcourseObject.thumbnail_url == '' || this.updateEcourseObject.thumbnail_url == null) && (this.thumbnail_image == '' || this.thumbnail_image == null)) {
+        this._msgService.showErrorMessage('error','', 'Please select Thumbnail or upload image');
+      } else {
+        if (this.jsonflag.isUpadted) {
         this.updateCourseMapping();
+      } else {
+        this.addEcourseMapping();
       }
+    }
 
     }
     } else {
       this.jsonflag.isAssignBatch = false;
       if (this.jsonflag.isUpadted) {
         this.updateCourseMapping();
+      } else {
+        this.addEcourseMapping();
       }
     }
   }
@@ -421,6 +507,56 @@ export class EcourseMappingComponent implements OnInit {
       });
       console.log(this.ecourseData);
     })
+  }
+
+  showAddEcourse() {
+    this.jsonflag.isUpadted = false;
+    this.getDefaultThumbnail();
+    this.statusOption = true;
+  }
+
+  setThumbnailUrl(obj) {
+    obj.isSelected = true;
+    this.updateEcourseObject.thumbnail_url = obj.data_value;
+  }
+
+  clearSelection() {
+    this.updateEcourseObject.thumbnail_url = null;
+    if(this.thumbnailData && this.thumbnailData.length) {
+      this.thumbnailData.forEach(obj => {
+        obj.isSelected = false;
+      })
+    }
+  }
+
+  fileUpload(imgId) {
+    let file = (<HTMLFormElement>document.getElementById('fileAdd')).files[0];
+    var myReader: FileReader = new FileReader();
+    let url : any;
+    myReader.readAsDataURL(file);
+    myReader.onloadend = () => {
+      this.thumbnail_image = (<string>myReader.result).split(',')[1];
+    }
+    myReader.onerror = function (error) {
+    };
+  }
+  getDefaultThumbnail() {
+    this.auth.showLoader();
+    const url = `/api/v1/masterData/key/ECOURSE_MAPPING_DEFAULT_IMAGES/IMAGE`;
+    this._http.getData(url).subscribe(
+      (res: any) => {
+        this.auth.hideLoader();
+        this.thumbnailData = res;
+        if(this.thumbnailData && this.thumbnailData.length) {
+          this.thumbnailData.forEach(obj => {
+            obj.isSelected = false;
+          })
+        }
+      },
+      err => {
+        this.auth.hideLoader();
+      }
+    )
   }
 
 }
