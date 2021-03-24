643b1e49eff06ebe955082453a1f96840de5d64f
manisha13j <manisha@proctur.com>
2021-03-24 19:20:40 +0530

    view-attendance changes

--- src/app/components/users-management/attendance/view-attendance/view-attendance.component.ts
+++ src/app/components/users-management/attendance/view-attendance/view-attendance.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { MessageShowService } from '../../../../services/message-show.service';
 import { HttpService } from '../../../../services/http.service';
-// import { document } from 'ngx-bootstrap-custome/utils/facade/browser';
 import { provideRoutes, Router } from '@angular/router';
 import { AuthenticatorService } from '../../../../services/authenticator.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-view-attendance',
@@ -13,16 +13,20 @@ import { AuthenticatorService } from '../../../../services/authenticator.service
 })
 export class ViewAttendanceComponent implements OnInit {
 
-  activeDayIsOpen: boolean;
-
+  userId: any
+  userName: any
+  dayStetus: any
+  nameData: any
   jsonFlag = {
     institute_id: ''
   }
   userDetailsAttendance = {
+    userName: '',
 
     fromDate: moment(new Date()).format('YYYY-MM-DD'),
     toDate: moment(new Date()).format('YYYY-MM-DD'),
-    user_id: '17863',
+    user_id: 0,
+    userPhoneNo: ''
 
   }
   //viewDate: Date = new Date();
@@ -32,12 +36,18 @@ export class ViewAttendanceComponent implements OnInit {
   constructor(private msgService: MessageShowService,
     private httpService: HttpService,
     private router: Router,
-    private auth: AuthenticatorService) {
+    private auth: AuthenticatorService, private route: ActivatedRoute) {
     this.jsonFlag.institute_id = sessionStorage.getItem('institution_id');
+    this.route.params.subscribe((res: any) => {
+
+      this.userId = res['id']
+    }
+    )
 
   }
 
   ngOnInit(): void {
+
     this.getallAttendanceDetails()
 
   }
@@ -45,12 +55,16 @@ export class ViewAttendanceComponent implements OnInit {
   getallAttendanceDetails() {
     this.auth.showLoader();
 
-    const url = '/api/v1/daily/attendance/' + this.jsonFlag.institute_id + /getByUser/ + this.userDetailsAttendance.user_id + '?' + 'fromDate=' + this.userDetailsAttendance.fromDate + '&&' + 'toDate=' + this.userDetailsAttendance.toDate;
+    const url = '/api/v1/daily/attendance/' + this.jsonFlag.institute_id + /getByUser/ + this.userId + '?' + 'fromDate=' + this.userDetailsAttendance.fromDate + '&&' + 'toDate=' + this.userDetailsAttendance.toDate;
     this.httpService.getData(url).subscribe(
       (res: any) => {
         this.auth.hideLoader();
-        this.userDetailsAttendanceList = res;
+        this.userDetailsAttendanceList = res.result;
+        this.dayStetus = res.daysStatus
+        this.nameData = res.result
+
         console.log("ASHA create Atttendance", this.userDetailsAttendanceList)
+
       },
       err => {
         this.auth.hideLoader();
