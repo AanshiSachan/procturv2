83cddcef0e8009e1817c4b89fc0bfdaa6f029887
Abhishek <abhishek026kumar@gmail.com>
2021-04-06 15:51:59 +0530

    remove debugger

--- src/app/components/student-module/student-batch-list/student-batch-list.component.ts
+++ src/app/components/student-module/student-batch-list/student-batch-list.component.ts
@@ -249,7 +249,6 @@ export class StudentBatchListComponent implements OnInit, OnChanges {
     }
 
     batchChangeAlert(value, batch) {
-        debugger
         if (this.checkForAssignSingleSection(value, batch.data.course_id)) {
             return;
         }
