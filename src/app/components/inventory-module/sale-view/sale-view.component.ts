90070237ea1c29387675ef313f38caef8f3f17cd
manishaj <manisha@proctur.com>
2021-04-19 23:12:55 +0530

    view-done

--- src/app/components/inventory-module/sale-view/sale-view.component.ts
+++ src/app/components/inventory-module/sale-view/sale-view.component.ts
@@ -40,8 +40,16 @@ getViewDataById(){
       this.auth.hideLoader();
     })
 }
-printData(){
-  window.print();
+printData(divName){
+  //window.print();
+  var printContents = document.getElementById(divName).innerHTML;
+			var originalContents = document.body.innerHTML;
+
+			document.body.innerHTML = printContents;
+
+			window.print();
+
+			document.body.innerHTML = originalContents;
 }
 
 }
