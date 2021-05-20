e35c515cd2ad8b7b716e67995db7b5b820eb09b7
naliniProctur <nalini@proctur.com>
2021-05-20 12:05:36 +0530

    Course phase 2

--- src/app/components/course-module/create-course/course-course-list-v2/course-add/course-add.component.ts
+++ src/app/components/course-module/create-course/course-course-list-v2/course-add/course-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../../../../services/http.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-course-add',
@@ -6,10 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-add.component.scss']
 })
 export class CourseAddComponent implements OnInit {
+  academicList:any=[];
+  courseDetails: any = {
+    course_name: '',
+    start_Date: '',
+    end_Date: '',
+    academic_year_id: '',
+    master_course_id: '',
+    inst_id:sessionStorage.getItem('institute_id')
+  };
 
-  constructor() { }
+  constructor(
+    private _httpService: HttpService
+  ) { }
 
   ngOnInit(): void {
+    this.getAcademicYearDetails();
   }
+
+  getAcademicYearDetails() {
+    this.academicList = [];
+    this._httpService.getData('/api/v1/academicYear/all/').subscribe(
+      res => {
+        this.academicList = res;
+      },
+      err => {
+      }
+    )
+  }
+
+  setStartAdEndDate(row) {
+    for (let acad of this.academicList) {
+      if (row == null) {
+        if (acad.default_academic_year == 1) {
+          // this.defaultAY=acad.inst_acad_year_id;
+          // this.defaultAYStartDate=moment(acad.start_date).format('YYYY-MM-DD');
+          // this.defaultAYEndDate=moment(acad.end_date).format('YYYY-MM-DD');
+          this.courseDetails.academic_year_id = acad.inst_acad_year_id;
+          this.courseDetails.start_Date = moment(acad.start_date).format('YYYY-MM-DD');
+          this.courseDetails.end_Date = moment(acad.end_date).format('YYYY-MM-DD');
+          break;
+        }
+      }
+      else if (acad.inst_acad_year_id == row.academic_year_id) {
+        row.start_Date = moment(acad.start_date).format('YYYY-MM-DD');
+        row.end_Date = moment(acad.end_date).format('YYYY-MM-DD');
+        break;
+      }
+      else if (row.academic_year_id == '-1') {
+        row.start_Date = ''
+        row.end_Date = ''
+        break;
+      }
+    }
+  }
+
 
 }
--- src/app/components/course-module/create-course/course-course-list-v2/course-course-list-v2.component.ts
+++ src/app/components/course-module/create-course/course-course-list-v2/course-course-list-v2.component.ts
@@ -13,15 +13,24 @@ export class CourseCourseListV2Component implements OnInit {
   master_course_id: any = '-1';
   masterCourseData: any = [];
   courseData: any = [];
+  standardList: any = [];
+  createMasterCourseModel={
+    "master_course_name": "",
+    "institute_id": this.institute_id,
+    "is_active": "Y",
+    "standard_id":'-1'
+}
+
 
   constructor(
     private _httpService: HttpService,
-    private _auth : AuthenticatorService,
-    private _msgService:MessageShowService
+    private _auth: AuthenticatorService,
+    private _msgService: MessageShowService
   ) { }
 
   ngOnInit(): void {
     this.getMasterCourseData();
+    this.fetchStandard();
   }
 
   getMasterCourseData() {
@@ -47,6 +56,35 @@ export class CourseCourseListV2Component implements OnInit {
         this.courseData = res.result;
       },
       (err: any) => {
+        this._auth.hideLoader();
+        this._msgService.showErrorMessage('error', '', err.error.message);
+      }
+    )
+  }
+
+  fetchStandard() {
+    let url = "/api/v1/standards/standard-subject-list/" + this.institute_id + '?is_active=Y';
+    this._auth.showLoader();
+    this._httpService.getData(url).subscribe(
+      (data: any) => {
+        this._auth.hideLoader();
+        this.standardList = data.result;
+      },
+      (error: any) => {
+        this._auth.hideLoader();
+        console.log(error);
+      }
+    )
+  }
+
+  createMasterCourse() {
+    this._auth.showLoader();
+    this._httpService.postData('/api/v1/master-course/create', this.createMasterCourseModel).subscribe(
+      (res:any)=>{
+        this._auth.hideLoader();
+        this._msgService.showErrorMessage('success','','Master course added successfully');
+      },
+      (err:any)=>{
         this._auth.hideLoader();
         this._msgService.showErrorMessage('error','',err.error.message);
       }
