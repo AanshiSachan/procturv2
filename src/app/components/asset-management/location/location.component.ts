c4899bdfde18fa1c52f9e4216f50120face49799
manishaj <manisha@proctur.com>
2021-06-22 13:58:24 +0530

    changes

--- src/app/components/asset-management/location/location.component.ts
+++ src/app/components/asset-management/location/location.component.ts
@@ -251,6 +251,23 @@ export class LocationComponent implements OnInit {
 
     }
     else {
+      //http://localhost:8081/prod/api/v2/asset/location/search?searchString=mumbai&instituteId=100075
+    
+      if(this.searchParams.length>=3){
+        this.auth.showLoader();
+        this.httpService.getMethod('api/v2/asset/location/search?searchString='+this.searchParams + '&instituteId='+this.model.institute_id, null).subscribe(
+          (res: any) => {
+            this.staticPageData = res.result.response;
+            this.tempLocationList = res.result.response;
+            this.totalRecords = res.result.total_elements;
+            this.auth.hideLoader();
+          },
+          err => {
+            this.auth.hideLoader();
+          }
+        );
+      }
+    
       let searchData = this.tempLocationList.filter(item =>
         Object.keys(item).some(
           k => item[k] != null && item[k].toString().toLowerCase().includes(this.searchParams.toLowerCase()))
