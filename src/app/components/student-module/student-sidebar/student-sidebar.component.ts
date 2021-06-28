f120aba2e3ab401e50b80cf324c7f8bd23e4320e
naliniProctur <nalini@proctur.com>
2021-06-28 16:50:32 +0530

    certificate changes

--- src/app/components/student-module/student-sidebar/student-sidebar.component.ts
+++ src/app/components/student-module/student-sidebar/student-sidebar.component.ts
@@ -610,7 +610,7 @@ characterPrintPage(popupName){
 }
 
 
-CertificateConvertTopdf(certificat){
+CertificateConvertTopdf(certificat,){
  
   var data = document.getElementById(certificat);  
   this.auth.showLoader()
@@ -618,15 +618,14 @@ CertificateConvertTopdf(certificat){
     html2canvas(data).then(canvas => {  
       var imgWidth = 230;   
       var pageHeight = 295;  
-     
       var imgHeight = canvas.height * imgWidth / canvas.width;  
       var heightLeft = imgHeight;  
 
       const contentDataURL = canvas.toDataURL('image/png') 
       let pdf = new jsPDF('p', 'mm', 'a4'); 
       var position = 0;  
-      
-      pdf.addImage(contentDataURL,'PNG', 0,position, imgWidth, imgHeight)  
+
+      pdf.addImage(contentDataURL,'PNG', 0,position, imgWidth, imgHeight,)  
       pdf.save('conductCertificates1.pdf');
       console.log("img",this.charactertCertiModel.logo_url) 
       
