7f282a27a6b367f564560667fd9100c2f37a498d
naliniProctur <nalini@proctur.com>
2021-08-10 19:53:56 +0530

    arp changes

--- src/app/components/website-configuration/arp-automation/arp-automation.component.ts
+++ src/app/components/website-configuration/arp-automation/arp-automation.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticatorService } from '../../../services/authenticator.service';
 import { MessageShowService } from '../../../services/message-show.service';
 import { HttpService } from '../../../services/http.service';
-import { type } from 'os';
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-arp-automation',
@@ -12,32 +13,34 @@ import { type } from 'os';
 export class ArpAutomationComponent implements OnInit {
   imgvarible={
     imgName:'',
+    height:0,
+    width:0
   
 
   }
   imgData:any=''
-  uploadedImg: any = '';
-  requestForBuil:any='';
+uploadedImg: any = '';
+ requestForBuil:string='';
 invalidEntry:any=''
+tokens:any=''
+newToken:any=''
+Authtokens:any=''
 appLogos:boolean=true
 generateApk:boolean=false
-fileToUpload: any;
-imageName:any
 selectedFile = null;
 image: File;
 imageModel:any
-buttonVariable:any
+buttonVariable:string=''
 currentStatusData:any=[]=[]
   constructor(private auth:AuthenticatorService,
     private _http: HttpService,private msgService: MessageShowService,
     ) {
-     //this.tokens =JSON.parse(sessionStorage.getItem('token'))
+     //this.Authtokens =sessionStorage.getItem('token')
      }
 
   ngOnInit(): void {
- //this.getToken()
-  this.currentStatusofClient()
- this.getAllImageResourse()
+ this.getToken()
+  //this.currentStatusofClient()
 
 
   
@@ -53,20 +56,18 @@ currentStatusData:any=[]=[]
  
   fileData: any;
 
-  onclickTab(id){
-    document.getElementById(id).click();
-
-  }
+ 
   getToken(){
     let url ='/api/v2/user/examdesk/SSO?source=ARP'
     this.auth.showLoader()
   this._http.getData(url).subscribe(
   (res:any)=>{
-    // console.log
-    //this.tokens =res.result;
+    this.tokens =res;
+    this.newToken=res.result;
+    //sessionStorage.setItem('token',JSON.stringify(this.newToken))
     this.currentStatusofClient()
-  // sessionStorage.setItem('token',JSON.stringify(this.tokens))
-
+    //this.getAllImageResourse()
+console.log("tokennnnnnnnnn",this.tokens)
     this.auth.hideLoader();
 
   },
@@ -79,21 +80,23 @@ currentStatusData:any=[]=[]
   )
   }
   currentStatusofClient(){
+
      let Params={
-    
-        // token:this.tokens
-       token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg"
-     
+
+     // token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg"
+     token : this.newToken
   }
-   
     this.auth.showLoader();
 let url ='client/requestStatus'
-  
 this._http.postARPData(url,Params).subscribe(
   (res:any)=>{
     this.currentStatusData = res.releaseDetails
-    this.buttonVariable = res.underRelease
-    this.requestForBuil = res.entry
+    if(res.underRelease == 1){
+    this.buttonVariable = 'some entry'
+    }
+    if(res.entry == 0){
+      this.requestForBuil = 'no entry'
+    }
     console.log("resssssss",res)
    
     this.auth.hideLoader();
@@ -114,49 +117,57 @@ this._http.postARPData(url,Params).subscribe(
     this.uploadedImg = img;
   }
  onImageSelect(event){
-this.selectedFile =<File> event.target.files[0]
+ 
+this.selectedFile = <File>event.target.files[0]
 console.log("image data",this.selectedFile)
+
+
  }
 
- 
 
 uploadHandler(){
-   
-  let obj={
-    
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg" ,
-   
+if(this.imagVladifation()){
+  let token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg"
 
-  }
   const formData = new FormData();
-  formData.append('data',JSON.stringify(obj)) 
-
-  formData.append('image', this.selectedFile)
-  this.auth.showLoader()
-  let url ='client/uploadResourceAPI'
-
-  this._http.postARPData(url,formData).subscribe(
-  (res:any)=>{
-    if (res.status == 200 ) {
-      this.msgService.showErrorMessage('success', '', "'image uploaded successfully");
-     
-      this.getAllImageResourse()
-    this.auth.hideLoader();
-    }
  
-  },
+  formData.append('token',token);
+
+  if (this.selectedFile) {
+    formData.append('file', this.selectedFile);
+ 
+}
+  let urlPostXlsDocument = 'http://arp3-api.proctur.com/v1/client/uploadResourceAPI'
+
+  let newxhr = new XMLHttpRequest();
+ newxhr.open("POST", urlPostXlsDocument, true);
+ newxhr.setRequestHeader("Authorization", token);
+ newxhr.setRequestHeader("Accept", "application/json, text/javascript");
+
+  newxhr.setRequestHeader("enctype", "multipart/form-data;");
+this.auth.showLoader();
+newxhr.onreadystatechange = () => {
+  this.auth.hideLoader();
+  if (newxhr.status == 200 ) {
+         this.msgService.showErrorMessage('success', '', 'image uploaded successfully')
+         this.getAllImageResourse()
+  }
+}
+newxhr.send(formData);
+
   err => {
     this.auth.hideLoader();
   
     this.msgService.showErrorMessage(this.msgService.toastTypes.error, '', err);
   
   }
-  )
+  
   }
+}
   getAllImageResourse(){
     let obj={
      
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg"
+      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg"
   
   
       
@@ -167,24 +178,7 @@ uploadHandler(){
     (res:any)=>{
       this.imgData=res
       console.log("resssssss imageeeeee", this.imgData)
-      // if (this.imgvarible.imgName == ' icon_48_url') {
-      //   this.togglerImg(' icon_48_url','48', '48', this.imageModel. icon_48_url);
-      // } else if (this.imgvarible.imgName == 'icon_96_url') {
-      //   this.togglerImg('icon_96_url', '96', '96', this.imageModel.icon_96_url);
-      // } else if (this.imgvarible.imgName== 'icon_144_url') {
-      //   this.togglerImg('icon_144_url', '144', '144',  this.imageModel.icon_144_url);
-      // } else if (this.imgvarible.imgName== 'icon_192_url') {
-      //   this.togglerImg('icon_192_url', '192', '192',  this.imageModel.icon_192_url);
-      // } else if (this.imgvarible.imgName== 'icon_512_url') {
-      //   this.togglerImg('icon_512_url', '512', '512',  this.imageModel.icon_512_url);
-      // } else if (this.imgvarible.imgName== 'icon_420_url') {
-      //   this.togglerImg('icon_420_url', '420', '420',  this.imageModel.icon_420_url);
-      // }  else if (this.imgvarible.imgName== 'splash_url') {
-      //   this.togglerImg('splash_url', '1025 ', '500',  this.imageModel.splash_url);
-      // }else if (this.imgvarible.imgName== 'banner_url') {
-      //   this.togglerImg('banner_url', '1800  ', '900 ',  this.imageModel.banner_url);
-      // }
-     
+   
 
       this.auth.hideLoader();
     },
@@ -200,7 +194,7 @@ uploadHandler(){
 reuestBuildApk(){
   let obj={
   
-    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0aXR1dGVfaWQiOjEwMDEyNiwiaWF0IjoxNjI2OTUzMjY0fQ.k2Ngi8Fvy9BXc5107Gifzv0Bf-lZkiKybRQ5tOUcpMg"
+    token:this.newToken
 
 
     }
@@ -223,6 +217,13 @@ err => {
 }
 )
 }
-
-  
+imagVladifation(){
+ 
+if(this.selectedFile.max_height !=900 || this.selectedFile.max_width != 900){
+  this.msgService.showErrorMessage(this.msgService.toastTypes.error, '', "Please upload file of size 900 * 900 px ");
+ return;
 }
+return true;
+}
+}
+
