87d33a000c555efbdf0b683f00433e86a47b318b
naliniProctur <nalini@proctur.com>
2021-08-30 19:34:48 +0530

    domain changes

--- src/app/components/website-configuration/url/url.component.ts
+++ src/app/components/website-configuration/url/url.component.ts
@@ -1,6 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
 import { AuthenticatorService } from '../../../services/authenticator.service';
 import { ProductService } from '../../../services/products.service';
+import { HttpService } from '../../../services/http.service';
+import { MessageShowService } from '../../../services/message-show.service';
+
 
 @Component({
   selector: 'app-url',
@@ -9,10 +13,25 @@ import { ProductService } from '../../../services/products.service';
 })
 export class UrlComponent implements OnInit {
   pageModel: any = '';
+  subDomainSystem:string='systemSubDomain';
+  domain:string='';
+  checkAvailable:string="";
+  editString:string="";
+  addNewDomain:string="";
+  addNewSubDomain:string="";
+  showPreview:string="";
+  domainModel={
+    subDomain_name:'proctur.com',
+    domain_name:''
 
+  
+  }
+availabiltyCheckData:any=[]=[]
   constructor(
     private productService: ProductService,
     private auth: AuthenticatorService,
+    private _http: HttpService,
+    private msgService:MessageShowService
   ) { }
 
   ngOnInit(): void {
@@ -28,11 +47,31 @@ export class UrlComponent implements OnInit {
         if(this.pageModel.subdomain_name != '' && !this.pageModel.subdomain_name.includes('https://')) {
           let temp = 'https://' + this.pageModel.subdomain_name;
           this.pageModel.subdomain_name = temp;
+      }
+// =====================new-added============================
+        if(this.pageModel.subdomain_name != '' && this.pageModel.subdomain_name.includes('proctur.com')){
+          this. addNewDomain ='Add-New-domains';
+          this.subDomainSystem = 'systemSubDomain'
+
+          this.domain=""
+
+        }else{
+          //this. domain =='Add-New-domains';
+          this.addNewSubDomain  = 'Add-New-SubDomain'
+          this.domain = 'domains'
+          this.subDomainSystem =""
+
         }
-        //this.fetchTableDataByPage(this.pageIndex);
+        
+      //   else if(this.pageModel.subdomain_name != '' && !this.pageModel.subdomain_name.includes('proctur.com')){
+      //    this. domain =='domains';
+        
+      // }
+      console.log("domain",this.pageModel)
       },
       err => {
         this.auth.hideLoader();
+
       }
     );
   }
@@ -43,4 +82,137 @@ export class UrlComponent implements OnInit {
     }
   }
 
+// =====================new-UI-functionality===========================
+checkAvailability(){
+  if(!this.domainModel.subDomain_name.includes('https://')){
+  let com = this.pageModel.subdomain_name.slice(8)
+    if(this.domainModel.subDomain_name != com){
+  this.auth.showLoader();
+  let url='/api/v1/institutes/subdomain/validate?name='+this.domainModel.subDomain_name+'&instituteId='+ sessionStorage.getItem('institute_id');
+  this._http.getData(url).subscribe(
+  (res: any) => {
+    this.auth.hideLoader();
+    this.availabiltyCheckData = res;
+    if(res.validate == true ){
+      this.checkAvailable ="available"
+    }
+    // if(res.result == false){
+    //   this.msgService.showErrorMessage('error','','Updated url already assigned to another client!');
+  
+    //  }
+  },
+  err => {
+    this.auth.hideLoader();
+    console.log(err.error.validate)
+    if(err.error.validate == false){
+    this.checkAvailable ='not-available'
+    }
+    // if(err.error.result == false){
+    //   this.msgService.showErrorMessage(this.msgService.toastTypes.error, '', err.error.message);
+  
+    // }
+    console.log("error",err)
+  }
+);
+    }else{
+      this.msgService.showErrorMessage('error','','Updated url already assigned to another client!');
+
+    }
+  }else{
+    this.msgService.showErrorMessage('error', '', 'Please enter valid url');
+
+  }
 }
+updateSubdomain(){
+  let obj={
+  old_domain:this.pageModel.subdomain_name !='' ? this.pageModel.subdomain_name.replace("https://",""):null,
+  updated_domain:this.domainModel.subDomain_name
+
+  }
+  this.auth.showLoader();
+  const url = `/api/v1/institutes/subdomain/update/${sessionStorage.getItem('institute_id')}`
+
+  this._http.putData(url,obj).subscribe(
+    (res: any) => {
+      this.auth.hideLoader();
+      this.msgService.showErrorMessage('success', '', "Url updated successfully");
+      this.domainModel.subDomain_name="proctur.com"
+      this.checkAvailable=""
+      this.editString=""
+      this.getData();
+
+    
+  },
+  err => {
+    this.auth.hideLoader();
+    this.msgService.showErrorMessage(this.msgService.toastTypes.error, '', err.error.message);
+    this.checkAvailable =""
+  }
+  )
+  
+}
+updateDomain(){
+  if(!this.domainModel.domain_name.includes("https://")){
+  let obj={
+    old_domain:this.pageModel.subdomain_name !='' ? this.pageModel.subdomain_name.replace("https://",""):null,
+    updated_domain:this.domainModel.domain_name
+  
+    }
+    this.auth.showLoader();
+    const url = `/api/v1/institutes/subdomain/update/${sessionStorage.getItem('institute_id')}`
+  
+    this._http.putData(url,obj).subscribe(
+      (res: any) => {
+        this.auth.hideLoader();
+        this.msgService.showErrorMessage('success', '', "Domain updated successfully");
+        this.domainModel.domain_name=""
+        this.editString=""
+        this.getData();
+  
+      
+    },
+    err => {
+      this.auth.hideLoader();
+      this.msgService.showErrorMessage(this.msgService.toastTypes.error, '', err.error.message);
+  
+    }
+    )
+    
+  }else{
+    this.msgService.showErrorMessage('error', '', 'Please enter valid url');
+
+  }
+}
+onclickEdit(obj){
+  this.editString='edit-url'
+  this.domainModel.subDomain_name=obj
+console.log("ediiiiiii",this.domainModel.subDomain_name)
+}
+
+onclickSubDomain(){
+this.subDomainSystem = "systemSubDomain";
+this.domain=''
+}
+onclickDomain(){
+  this.domain="domains";
+  this.subDomainSystem='';
+this.addNewDomain ='Add-New-domains'
+this.editString=""
+
+}
+clearSubdomainDiv(){
+  this.domain=''
+  this.editString=""
+}
+clearDomain(){
+  this.subDomainSystem='';
+  this.editString=""
+}
+onclickCancel(){
+  this.domainModel.domain_name=""
+  this.domainModel.subDomain_name=""
+  this.editString=""
+
+}
+}
+
